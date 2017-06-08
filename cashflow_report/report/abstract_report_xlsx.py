b9976d144f57b1f891406b122b4a2846afff7140
Luiz Felipe do Divino <luiz.divino@kmee.com.br>
2017-06-08 19:13:09 -0300

    [ADD] linhas do resumo da soma das contas no relatorio de fluxo de caixa

--- cashflow_report/report/abstract_report_xlsx.py
+++ cashflow_report/report/abstract_report_xlsx.py
@@ -46,6 +46,7 @@ class AbstractReportXslx(ReportXlsx):
         report_name = self._get_report_name()
         # filters = self._get_report_filters(report)
         self.columns = self._get_report_columns(report)
+        self.columns_resumo = self._get_report_columns_resumo(report)
 
         self.sheet = workbook.add_worksheet(report_name[:31])
 
@@ -158,6 +159,15 @@ class AbstractReportXslx(ReportXlsx):
         Columns are defined with `_get_report_columns` method.
         """
         for col_pos, column in self.columns.iteritems():
+            self.sheet.write(self.row_pos, col_pos, column['header'],
+                             self.format_header_center)
+        self.row_pos += 1
+
+    def write_array_header_resumo(self):
+        """Write array header on current line using all defined columns name.
+        Columns are defined with `_get_report_columns` method.
+        """
+        for col_pos, column in self.columns_resumo.iteritems():
             self.sheet.write(self.row_pos, col_pos, column['header'],
                              self.format_header_center)
         self.row_pos += 1
--- cashflow_report/report/trial_balance_xlsx.py
+++ cashflow_report/report/trial_balance_xlsx.py
@@ -38,6 +38,25 @@ class TrialBalanceXslx(abstract_report_xlsx.AbstractReportXslx):
 
         return result
 
+    def _get_report_columns_resumo(self, report):
+
+            row_mes = 2
+
+            result = {
+                1: {'header': _('Resumo'), 'field': '1', 'type': 'amount',
+                    'width': 20},
+            }
+
+            for mes in self.env.context['fluxo_de_caixa']['meses']:
+                result.update({
+                    row_mes: {'header': mes, 'field': row_mes, 'type': 'float',
+                              'width': 10},
+                })
+
+                row_mes += 1
+
+            return result
+
         # for col_pos, column in self.columns.iteritems():
         #     value = getattr(line_object, column['field'])
         #     cell_type = column.get('type', 'string')
@@ -68,6 +87,25 @@ class TrialBalanceXslx(abstract_report_xlsx.AbstractReportXslx):
             # else:
         self.row_pos += 1
 
+    def write_line_resumo(self, line_object):
+        """Write a line on current line using all defined columns field name.
+        Columns are defined with `_get_report_columns` method.
+        """
+        for col_pos, column in self.columns_resumo.iteritems():
+
+            value = line_object[int(column.get('field'))]
+
+            if column.get('type') == 'float':
+                self.sheet.write_number(self.row_pos, col_pos, value)
+            else:
+                self.sheet.write_string(self.row_pos, col_pos, value or '')
+
+            # cell_type = column.get('type', 'string')
+            # if column.get('field') == 'relacional':
+            #     self.sheet.write_string(self.row_pos, col_pos, value or '')
+            # else:
+        self.row_pos += 1
+
     def _get_col_count_filter_name(self):
         return 2
 
@@ -76,34 +114,19 @@ class TrialBalanceXslx(abstract_report_xlsx.AbstractReportXslx):
 
     def _generate_report_content(self, workbook, report):
 
-        if not report.show_partner_details:
-            # Display array header for account lines
-            self.write_array_header()
+        self.write_array_header()
 
         # For each account
         # for account in report.fluxo_de_caixa['']:
         for account in self.env.context['fluxo_de_caixa']['contas']:
             # Display account lines
             self.write_line(account)
-        #
-        #     else:
-        #         # Write account title
-        #         self.write_array_title(account.code + ' - ' + account.name)
-        #
-        #         # Display array header for partner lines
-        #         self.write_array_header()
-        #
-        #         # For each partner
-        #         for partner in account.partner_ids:
-        #             # Display partner lines
-        #             self.write_line(partner)
-        #
-        #         # Display account footer line
-        #         self.write_account_footer(account,
-        #                                   account.code + ' - ' + account.name)
-        #
-        #         # Line break
-        #         self.row_pos += 2
+
+        self.write_array_header_resumo()
+
+        for resumo in self.env.context['fluxo_de_caixa']['resumo']:
+            # Display account lines
+            self.write_line_resumo(resumo[1:])
 
     # def write_account_footer(self, account, name_value):
     #     """Specific function to write account footer for Trial Balance"""

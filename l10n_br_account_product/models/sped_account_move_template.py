3820bed894b1f14cee8ea80d2d8e8df39dcf1ee6
Ari Caldeira <aristides.caldeira@kmee.com.br>
2017-07-17 10:24:30 -0300

    [NEW] Backport gera_account_move in sped.account.move.template

--- l10n_br_account_product/models/sped_account_move_template.py
+++ l10n_br_account_product/models/sped_account_move_template.py
@@ -47,3 +47,34 @@ class SpedAccountMoveTemplate(models.Model):
     #         if len(operacao.account_move_template_ids) > 1:
     #             raise UserError(u'A operação fiscal %s já tem um
     # modelo de partidas dobradas vinculado!' % operacao.nome)
+
+    @api.multi
+    def gera_account_move(self):
+        for documento in self:
+            dados = {
+                'sped_documento_id': documento.id,
+                'journal_id': documento.journal_id.id,
+                'ref': documento.descricao,
+                'sped_participante_id': documento.participante_id.id,
+                'sped_empresa_id': documento.empresa_id.id,
+                'partner_id': documento.participante_id.partner_id.id,
+                'company_id': documento.empresa_id.company_id.id,
+                'date': documento.data_entrada_saida,
+            }
+
+            if documento.account_move_id:
+                if documento.account_move_id.state != 'draft':
+                    # raise
+                    continue
+                account_move = documento.account_move_id
+                account_move.write(dados)
+            else:
+                account_move = self.env['account.move'].create(dados)
+                documento.account_move_id = account_move
+
+            line_ids = [(5, 0, {})]
+
+            documento.item_ids.gera_account_move_line(account_move,
+                documento.account_move_template_id, line_ids)
+
+            account_move.write({'line_ids': line_ids})

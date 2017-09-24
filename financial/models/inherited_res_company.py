beed4c8efbb5898c244a945b43f24be2baca64f0
Wagner Pereira <wagner.pereira@kmee.com.br>
2017-09-24 09:31:42 -0300

    [FIX] Corrige cron que atualizada date_today

--- financial/models/inherited_res_company.py
+++ financial/models/inherited_res_company.py
@@ -12,6 +12,7 @@ class ResCompany(models.Model):
         string='Today Date',
     )
 
+    @api.model
     def cron_update_reference_date_today(self):
         for company in self.env['res.company'].search([]):
             company.today_date = fields.Date.today()

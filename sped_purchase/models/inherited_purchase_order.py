06df82fbe6b7a70eae677376ca1f4bbea88a4f7e
Isabella Maia <isabella.maia@kmee.com.br>
2017-10-11 16:59:55 -0300

    [MIG][sped_purchase] 2to3

--- sped_purchase/models/inherited_purchase_order.py
+++ sped_purchase/models/inherited_purchase_order.py
@@ -2,7 +2,7 @@
 # Copyright 2017 KMEE
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from __future__ import division, print_function, unicode_literals
+
 
 from odoo import api, fields, models, _
 from odoo.addons.sped_imposto.models.sped_calculo_imposto import (

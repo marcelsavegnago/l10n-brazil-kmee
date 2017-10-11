04d9778ebbb428288b361fde6e855db97c5aee1a
Isabella Maia <isabella.maia@kmee.com.br>
2017-10-11 15:46:57 -0300

    [MIG][sped_stock] 2to3

--- sped_stock/models/inherited_stock_move.py
+++ sped_stock/models/inherited_stock_move.py
@@ -2,7 +2,7 @@
 # Copyright 2017 KMEE
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from __future__ import division, print_function, unicode_literals
+
 
 import logging
 
@@ -26,7 +26,7 @@ class StockMove(SpedCalculoImpostoItem, models.Model):
     _abstract = False
 
     is_brazilian = fields.Boolean(
-        string=u'Is a Brazilian Invoice?',
+        string='Is a Brazilian Invoice?',
         related='picking_id.is_brazilian',
     )
     #

c7e221ed6effcbfc32ff2b03b89f3cf11058c1f7
Wagner Pereira <wagner.pereira@kmee.com.br>
2017-10-16 11:44:40 -0200

    [ADD] Tabelas eSocial 01, 02, 03, 13, 14, 15, 16, 17, 18 , 19, 20 , 21, 25 e 26

---
+++ models/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2016 KMEE - Luis Felipe Miléo <mileo@kmee.com.br>
+# Copyright 2016 KMEE - Hendrix Costa <hendrix.costa@kmee.com.br>
+# Copyright 2017 KMEE - Aristides Caldeira <aristides.caldeira@kmee.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+#
+
+from . import categoria_trabalhador
+from . import financiamento_aposentadoria
+from . import natureza_rubrica
+from . import parte_corpo
+from . import agente_causador
+from . import situacao_geradora_doenca
+from . import situacao_geradora_acidente
+from . import natureza_lesao
+from . import motivo_afastamento
+from . import motivo_desligamento
+from . import tipo_logradouro
+from . import natureza_juridica
+from . import motivo_cessacao
+from . import tipo_beneficio

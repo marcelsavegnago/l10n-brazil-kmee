d407edb484c581501902f738374b46525a7c5265
Wagner Pereira <wagner.pereira@kmee.com.br>
2017-10-16 11:44:40 -0200

    [ADD] Tabelas eSocial 01, 02, 03, 13, 14, 15, 16, 17, 18 , 19, 20 , 21, 25 e 26

---
+++ models/natureza_rubrica.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017 KMEE
+#   Wagner Pereira <wagner.pereira@kmee.com.br>
+# License AGPL-3 or later (http://www.gnu.org/licenses/agpl)
+#
+
+from odoo import api, fields, models, _
+
+
+class NaturezaRubrica(models.Model):
+    _name = 'esocial.natureza_rubrica'
+    _description = 'Natureza das Rubricas da Folha de Pagamento'
+    _order = 'name'
+    _sql_constraints = [
+        ('codigo',
+         'unique(codigo)',
+         'Este código já existe !'
+         )
+    ]
+
+    codigo = fields.Char(
+        size=4,
+        string='Codigo',
+        required=True,
+    )
+    nome = fields.Char(
+        string='Nome',
+        required=True,
+    )
+    descricao = fields.Char(
+        string='Descrição',
+    )
+    name = fields.Char(
+        compute='_calcula_name',
+        store=True,
+    )
+
+    @api.onchange('codigo')
+    def _valida_codigo(self):
+        for natureza in self:
+            if natureza.codigo:
+                if natureza.codigo.isdigit():
+                    natureza.codigo = natureza.codigo.zfill(4)
+                else:
+                    res = {'warning': {
+                        'title': _('Código Incorreto!'),
+                        'message': _('Campo Código somente aceita números! - Corrija antes de salvar')
+                    }}
+                    natureza.codigo = False
+                    return res
+
+    @api.depends('codigo', 'nome')
+    def _calcula_name(self):
+        for natureza in self:
+            natureza.name = natureza.codigo + '-' + natureza.nome

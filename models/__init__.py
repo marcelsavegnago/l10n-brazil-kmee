e4858df7d73a8b73d59de639971250fd85efc70a
Wagner Pereira <wagner.pereira@kmee.com.br>
2017-10-23 11:45:28 -0200

    [FIX] Copyrights

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2016 KMEE - Luis Felipe Miléo <mileo@kmee.com.br>
-# Copyright 2016 KMEE - Hendrix Costa <hendrix.costa@kmee.com.br>
-# Copyright 2017 KMEE - Aristides Caldeira <aristides.caldeira@kmee.com.br>
+# Copyright 2017 KMEE - Átila Graciano <atilla.silva@kmee.com.br>
+# Copyright 2017 KMEE - Bianca Bartolomei <bianca.bartolomei@kmee.com.br>
+# Copyright 2017 KMEE - Wagner Pereira <wagner.pereira@kmee.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 #
 

1456bbaff94fcc263c06718b81001a2d2261d7d0
Luis Felipe Mileo <mileo@kmee.com.br>
2017-10-24 23:46:15 -0200

    fixup! [NEW] Commit inicial do sped fiscal

--- sped_fiscal/__manifest__.py
+++ sped_fiscal/__manifest__.py
@@ -11,6 +11,7 @@
     'author': 'KMEE,Odoo Community Association (OCA)',
     'website': 'www.kmee.com.br',
     'depends': [
+        'sped',
     ],
     'data': [
         'security/sped_fiscal.xml',
--- sped_fiscal/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import sped_fiscal

1e05a2807dd26c307bf3c3c9514ffeb6f2672313
Ari Caldeira <ari.caldeira@tauga.com.br>
2017-11-24 16:03:03 -0200

    [FIX] Criando primeiro os grupos de permissão

--- sped_imposto/__manifest__.py
+++ sped_imposto/__manifest__.py
@@ -22,6 +22,13 @@
     ],
     'data': [
         #
+        # Grupos e permissões
+        #
+        'security/inherited_res_groups_data.xml',
+        'security/ir.model.access.csv',
+        'security/sped_operacao_ir_rule.xml',
+
+        #
         # Valores padrão
         #
         'data/sped_aliquota_icms_proprio_data.xml',
@@ -84,13 +91,6 @@
         # Recebimento de documento
         #
         'views/sped_operacao_recebimento_nfe_view.xml',
-
-        #
-        # Grupos e permissões
-        #
-        'security/inherited_res_groups_data.xml',
-        'security/ir.model.access.csv',
-        'security/sped_operacao_ir_rule.xml',
     ],
     'demo': [
         'demo/sped_produto_demo.xml',

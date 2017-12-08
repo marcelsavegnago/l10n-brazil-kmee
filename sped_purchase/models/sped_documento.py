9945ecee73d412fc1f1a04702ee869c4c8feb8b0
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2017-12-08 17:09:07 -0200

    [ADD] Importação da NF-e associando pedido de compra

--- sped_purchase/models/sped_documento.py
+++ sped_purchase/models/sped_documento.py
@@ -30,76 +30,84 @@ class SpedDocumento(models.Model):
         }
         return dados
 
-    # Carregar linhas da Purchase Order
-    @api.onchange('purchase_order_ids')
-    def purchase_order_change(self):
-        if not self.purchase_order_ids:
-            return {}
-        elif len(self.purchase_order_ids) == 1:
-            dados = {
-                'empresa_id': self.purchase_order_ids.empresa_id.id,
-                'operacao_id': self.purchase_order_ids.operacao_id.id,
-                'modelo': self.purchase_order_ids.operacao_id.modelo,
-                'emissao': self.purchase_order_ids.operacao_id.emissao,
-                'participante_id': self.purchase_order_ids.participante_id.id,
-                'condicao_pagamento_id': self.purchase_order_ids.condicao_pagamento_id.id if \
-                    self.purchase_order_ids.condicao_pagamento_id else False,
-                'transportadora_id': self.purchase_order_ids.transportadora_id.id if \
-                    self.purchase_order_ids.transportadora_id else False,
-                'modalidade_frete': self.purchase_order_ids.modalidade_frete,
-            }
-            dados.update(self.purchase_order_ids.prepara_dados_documento())
-            self.update(dados)
-            self.update(self._onchange_empresa_id()['value'])
-            self.update(self._onchange_operacao_id()['value'])
-    
-            if self.purchase_order_ids.presenca_comprador:
-                self.presenca_comprador = self.purchase_order_ids.presenca_comprador
-    
-            self.update(self._onchange_serie()['value'])
-            self.update(self._onchange_participante_id()['value'])
+    # # Carregar linhas da Purchase Order
+    # @api.onchange('purchase_order_ids')
+    # def purchase_order_change(self):
+    #     if not self.purchase_order_ids:
+    #         return {}
+    #     elif len(self.purchase_order_ids) == 1:
+    #         dados = {
+    #             'empresa_id': self.purchase_order_ids.empresa_id.id,
+    #             'operacao_id': self.purchase_order_ids.operacao_id.id,
+    #             'modelo': self.purchase_order_ids.operacao_id.modelo,
+    #             'emissao': self.purchase_order_ids.operacao_id.emissao,
+    #             'participante_id': self.purchase_order_ids.participante_id.id,
+    #             'condicao_pagamento_id': self.purchase_order_ids.condicao_pagamento_id.id if \
+    #                 self.purchase_order_ids.condicao_pagamento_id else False,
+    #             'transportadora_id': self.purchase_order_ids.transportadora_id.id if \
+    #                 self.purchase_order_ids.transportadora_id else False,
+    #             'modalidade_frete': self.purchase_order_ids.modalidade_frete,
+    #         }
+    #         dados.update(self.purchase_order_ids.prepara_dados_documento())
+    #         self.update(dados)
+    #         self.update(self._onchange_empresa_id()['value'])
+    #         self.update(self._onchange_operacao_id()['value'])
+    #
+    #         if self.purchase_order_ids.presenca_comprador:
+    #             self.presenca_comprador = self.purchase_order_ids.presenca_comprador
+    #
+    #         self.update(self._onchange_serie()['value'])
+    #         self.update(self._onchange_participante_id()['value'])
+    #
+    #     for pedido in self.mapped('purchase_order_ids'):
+    #         for item in pedido.order_line - \
+    #                 self.item_ids.mapped('purchase_line_ids'):
+    #             dados = self._preparar_sped_documento_item(item)
+    #             contexto = {
+    #                 'forca_vr_unitario': dados['vr_unitario']
+    #             }
+    #             documento_item = self.item_ids.mesclar_linhas(
+    #                 dados, item, contexto
+    #             )
+    #             if documento_item:
+    #                 self.item_ids += documento_item
+    #                 self.item_ids[-1].update(
+    #                     item.prepara_dados_documento_item()
+    #                 )
+    #     return {}
 
-        for pedido in self.mapped('purchase_order_ids'):
-            for item in pedido.order_line - \
-                    self.item_ids.mapped('purchase_line_ids'):
-                dados = self._preparar_sped_documento_item(item)
-                contexto = {
-                    'forca_vr_unitario': dados['vr_unitario']
-                }
-                documento_item = self.item_ids.mesclar_linhas(
-                    dados, item, contexto
-                )
-                if documento_item:
-                    self.item_ids += documento_item
-                    self.item_ids[-1].update(
-                        item.prepara_dados_documento_item()
-                    )
-        return {}
+    # @api.onchange('purchase_order_ids')
+    # def purchase_order_change(self):
+    #     self.ensure_one()
+    #     if not self.purchase_order_ids or len(self .mapped('purchase_order_ids')) > 1:
+    #         return {}
+    #     for item in self.mapped('item_ids'):
+    #         item.write({'purchase_ids': [(6, False, self.purchase_order_ids.ids)]})
 
-    @api.onchange('participante_id', 'item_ids')
-    def _onchange_allowed_purchase_ids(self):
-        result = {}
-
-        # A PO can be selected only if at least one PO line is not already in the invoice
-        purchase_line_ids = self.item_ids.mapped('purchase_line_ids')
-        purchase_ids = self.item_ids.mapped('purchase_ids').filtered(lambda r: r.order_line <= purchase_line_ids)
-
-        result['domain'] = {'purchase_order_ids': [
-            ('invoice_status', '=', 'to invoice'),
-            ('id', 'not in', purchase_ids.ids),
-        ]}
-
-        if self.participante_id:
-            result['domain']['purchase_order_ids'].append(
-                ('participante_id', '=', self.participante_id.id)
-            )
-
-        if self.empresa_id:
-            result['domain']['purchase_order_ids'].append(
-                ('empresa_id', '=', self.empresa_id.id)
-            )
-
-        return result
+    # @api.onchange('participante_id', 'item_ids.purchase_ids', 'item_ids.purchase_line_ids', 'empresa_id')
+    # def _onchange_allowed_purchase_ids(self):
+    #     result = {}
+    #
+    #     # A PO can be selected only if at least one PO line is not already in the invoice
+    #     purchase_line_ids = self.item_ids.mapped('purchase_line_ids')
+    #     purchase_ids = self.item_ids.mapped('purchase_ids').filtered(lambda r: r.order_line <= purchase_line_ids)
+    #
+    #     result['domain'] = {'purchase_order_ids': [
+    #         ('invoice_status', '=', 'to invoice'),
+    #         ('id', 'not in', purchase_ids.ids),
+    #     ]}
+    #
+    #     if self.participante_id:
+    #         result['domain']['purchase_order_ids'].append(
+    #             ('participante_id', '=', self.participante_id.id)
+    #         )
+    #
+    #     if self.empresa_id:
+    #         result['domain']['purchase_order_ids'].append(
+    #             ('empresa_id', '=', self.empresa_id.id)
+    #         )
+    #
+    #     return result
 
     def _criar_picking_entrada(self):
         if not self.purchase_order_ids:
--- sped_purchase/models/sped_documento_item.py
+++ sped_purchase/models/sped_documento_item.py
@@ -29,87 +29,97 @@ class SpedDocumentoItem(models.Model):
     )
 
     def faturar_linhas(self):
-        for linha in self.mapped('purchase_line_ids'):
+        if len(self.mapped('purchase_line_ids')) > 1:
+            for linha in self.mapped('purchase_line_ids'):
+                linha.qty_invoiced = linha.quantidade
+        elif self.purchase_line_ids:
             total = 0.0
-            for qty in linha.documento_item_ids.mapped('quantidade'):
-                total += qty
-            linha.qty_invoiced = total
+            for item in self.purchase_line_ids.mapped('documento_item_ids'):
+                total += item.quantidade
+            self.purchase_line_ids.qty_invoiced = total
 
     def executa_depois_create(self):
         for item in self:
             item.faturar_linhas()
 
     @api.multi
-    def mesclar_linhas(self, dados, line, contexto):
-        for linha in self:
-            mesclar = True
-            for item in dados.keys():
-                if item == 'quantidade' or linha[item] == dados[item]:
-                    continue
-                elif isinstance(linha[item], models.Model) and \
-                                linha[item].id == dados[item]:
-                    continue
-                else:
-                    mesclar = False
-            if mesclar:
-                linha.quantidade += dados.get('quantidade')
-                linha.purchase_line_ids += line
-                linha.purchase_ids += line.order_id
-                return []
+    def write(self, vals):
+        res = super(SpedDocumentoItem, self).write(vals)
+        self.executa_depois_create()
+        return res
 
-        return self.with_context(contexto).new(dados)
+    # @api.multi
+    # def mesclar_linhas(self, dados, line, contexto):
+    #     for linha in self:
+    #         mesclar = True
+    #         for item in dados.keys():
+    #             if item == 'quantidade' or linha[item] == dados[item]:
+    #                 continue
+    #             elif isinstance(linha[item], models.Model) and \
+    #                             linha[item].id == dados[item]:
+    #                 continue
+    #             else:
+    #                 mesclar = False
+    #         if mesclar:
+    #             linha.quantidade += dados.get('quantidade')
+    #             linha.purchase_line_ids += line
+    #             linha.purchase_ids += line.order_id
+    #             return []
+    #
+    #     return self.with_context(contexto).new(dados)
 
-    @api.onchange('purchase_ids', 'purchase_line_ids')
-    def _onchange_purchase_line_ids(self):
-        result = {}
-
-        # A PO can be selected only if at least one PO line is not already in the invoice
-        purchase_line_ids = self.purchase_ids.mapped('order_line')
-
-        result['domain'] = {'purchase_line_ids': [
-            ('id', 'in', purchase_line_ids.ids),
-            ('id', 'not in', self.purchase_line_ids.ids),
-        ], 'purchase_ids': [
-            ('invoice_status', '=', 'to invoice'),
-            ('id', 'not in', self.purchase_ids.ids),
-        ]}
-
-        if self.participante_id:
-            result['domain']['purchase_line_ids'].append(
-                ('participante_id', '=', self.participante_id.id)
-            )
-            result['domain']['purchase_ids'].append(
-                ('participante_id', '=', self.participante_id.id)
-            )
-
-        if self.empresa_id:
-            result['domain']['purchase_line_ids'].append(
-                ('empresa_id', '=', self.empresa_id.id)
-            )
-            result['domain']['purchase_ids'].append(
-                ('empresa_id', '=', self.empresa_id.id)
-            )
-
-        if self.produto_id:
-            result['domain']['purchase_line_ids'].append(
-                ('produto_id', '=', self.produto_id.id)
-            )
-
-        if self.vr_unitario:
-            result['domain']['purchase_line_ids'].append(
-                ('vr_unitario', '=', self.vr_unitario)
-            )
-
-        if len(self.purchase_line_ids) > 1:
-            self.pode_alterar_quantidade = False
-            quantidade = 0
-            for linha in self.purchase_line_ids:
-                quantidade += linha.quantidade
-            self.quantidade = quantidade
-        else:
-            self.pode_alterar_quantidade = True
-
-        return result
+    # @api.onchange('purchase_ids', 'purchase_line_ids')
+    # def _onchange_purchase_line_ids(self):
+    #     result = {}
+    #
+    #     # A PO can be selected only if at least one PO line is not already in the invoice
+    #     purchase_line_ids = self.purchase_ids.mapped('order_line')
+    #
+    #     if purchase_line_ids:
+    #         result['domain'] = {'purchase_line_ids': [
+    #             ('id', 'in', purchase_line_ids.ids),
+    #             ('id', 'not in', self.purchase_line_ids.ids),
+    #         ], 'purchase_ids': [
+    #             ('invoice_status', '=', 'to invoice'),
+    #             ('id', 'not in', self.purchase_ids.ids),
+    #         ]}
+    #
+    #     if self.participante_id:
+    #         result['domain']['purchase_line_ids'].append(
+    #             ('participante_id', '=', self.participante_id.id)
+    #         )
+    #         result['domain']['purchase_ids'].append(
+    #             ('participante_id', '=', self.participante_id.id)
+    #         )
+    #
+    #     if self.empresa_id:
+    #         result['domain']['purchase_line_ids'].append(
+    #             ('empresa_id', '=', self.empresa_id.id)
+    #         )
+    #         result['domain']['purchase_ids'].append(
+    #             ('empresa_id', '=', self.empresa_id.id)
+    #         )
+    #
+    #     if self.produto_id:
+    #         result['domain']['purchase_line_ids'].append(
+    #             ('produto_id', '=', self.produto_id.id)
+    #         )
+    #
+    #     if self.vr_unitario:
+    #         result['domain']['purchase_line_ids'].append(
+    #             ('vr_unitario', '=', self.vr_unitario)
+    #         )
+    #
+    #     if len(self.purchase_line_ids) > 1:
+    #         self.pode_alterar_quantidade = False
+    #         quantidade = 0
+    #         for linha in self.purchase_line_ids:
+    #             quantidade += linha.quantidade
+    #         self.quantidade = quantidade
+    #     else:
+    #         self.pode_alterar_quantidade = True
+    #
+    #     return result
 
     @api.onchange('quantidade')
     def constrains_quantidade(self):

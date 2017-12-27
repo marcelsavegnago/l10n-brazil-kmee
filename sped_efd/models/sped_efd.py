177d539a8ab4c15d9f17367672d01b70a150f997
Atilla Silva <atillaa4@hotmail.com>
2017-12-27 10:16:24 -0200

    [WIP] fluxo de validacao

--- sped_efd/models/sped_efd.py
+++ sped_efd/models/sped_efd.py
@@ -2,14 +2,14 @@
 import base64
 import datetime
 
-from sped.efd.icms_ipi import arquivos,registros
+from sped.efd.icms_ipi import arquivos, registros
 from odoo import fields, models, api
 
 from l10n_br_base.constante_tributaria import MODELO_FISCAL_NFE, MODELO_FISCAL_NFCE
 
 
 class SpedEFD(models.Model):
-    _name='sped.efd'
+    _name = 'sped.efd'
 
     company_id = fields.Many2one(
         comodel_name='res.company',
@@ -28,7 +28,7 @@ class SpedEFD(models.Model):
         default=fields.Datetime.now,
         required=True,
     )
-    dt_fim =  fields.Datetime(
+    dt_fim = fields.Datetime(
         string='Data final',
         index=True,
         required=True,
@@ -41,15 +41,15 @@ class SpedEFD(models.Model):
 
         return '009'
 
-    def transforma_data(self, data): # aaaammdd
+    def transforma_data(self, data):  # aaaammdd
         data = self.limpa_formatacao(data)
         return data[6:] + data[4:6] + data[:4]
 
     def limpa_formatacao(self, data):
         if data:
-            replace = ['-',' ','(',')','/','.',':']
+            replace = ['-', ' ', '(', ')', '/', '.', ':']
             for i in replace:
-                data = data.replace(i,'')
+                data = data.replace(i, '')
 
         return data
 
@@ -58,8 +58,8 @@ class SpedEFD(models.Model):
 
     def query_registro0000(self):
         query = """
-            select DISTINCT 
-                p.nome, p.cnpj_cpf, m.estado, p.ie, m.codigo_ibge, p.im, p.suframa
+            select DISTINCT
+                p.id, m.id 
             from 
                 sped_documento as d 
                 join sped_empresa as e on d.empresa_id=e.id
@@ -70,63 +70,29 @@ class SpedEFD(models.Model):
         """ % (self.company_id.id)
         self._cr.execute(query)
         query_resposta = self._cr.fetchall()
-
+        resposta_participante = self.env['sped.participante'].browse(query_resposta[0][0])
+        resposta_municipio = self.env['sped.municipio'].browse(query_resposta[0][1])
         registro_0000 = registros.Registro0000()
         registro_0000.COD_VER = str(self.versao)
-        registro_0000.COD_FIN = '0' # finalidade
-        registro_0000.DT_INI = self.transforma_data(self.dt_ini[:10]) # data_inicio
-        registro_0000.DT_FIN = self.transforma_data(self.dt_fim[:10]) # data_final
-        registro_0000.NOME = query_resposta[0][0] # filial.razao_social (?)
-        cpnj_cpf = self.limpa_formatacao(query_resposta[0][1])
+        registro_0000.COD_FIN = '0'  # finalidade
+        registro_0000.DT_INI = self.transforma_data(self.dt_ini[:10])  # data_inicio
+        registro_0000.DT_FIN = self.transforma_data(self.dt_fim[:10])  # data_final
+        registro_0000.NOME = resposta_participante.nome  # filial.razao_social (?)
+        cpnj_cpf = self.limpa_formatacao(resposta_participante.cnpj_cpf)
         if len(cpnj_cpf) == 11:
             registro_0000.CPF = cpnj_cpf
         else:
             registro_0000.CNPJ = cpnj_cpf
-<<<<<<< HEAD
-        registro_0000.UF = query_resposta[0][2] # filial.estado
-        registro_0000.IE = '11111111111111' # Todo: query_resposta[0][3] nao possui valor IE
-        registro_0000.COD_MUN = self.formata_cod_municipio(query_resposta[0][4]) # filial.municipio_id.codigo_ibge[:7]
-        registro_0000.IM = query_resposta[0][5] # filial.im
-        registro_0000.SUFRAMA = self.limpa_formatacao(query_resposta[0][6]) # filial.suframa
-=======
-        registro_0000.UF = resposta_municipio.estado # filial.estado
+        registro_0000.UF = resposta_municipio.estado  # filial.estado
         registro_0000.IE = self.limpa_formatacao(resposta_participante.ie)
-        registro_0000.COD_MUN = self.formata_cod_municipio(resposta_municipio.codigo_ibge) # filial.municipio_id.codigo_ibge[:7]
-        registro_0000.IM = resposta_participante.im # filial.im
-        registro_0000.SUFRAMA = self.limpa_formatacao(resposta_participante.suframa) # filial.suframa
->>>>>>> ead0597... [WIP] blocoC170, melhorias no codigo
-        registro_0000.IND_PERFIL = 'A' # perfil
-        registro_0000.IND_ATIV = '1' # tipo_atividade
-
+        registro_0000.COD_MUN = self.formata_cod_municipio(
+            resposta_municipio.codigo_ibge)  # filial.municipio_id.codigo_ibge[:7]
+        registro_0000.IM = resposta_participante.im  # filial.im
+        registro_0000.SUFRAMA = self.limpa_formatacao(resposta_participante.suframa)  # filial.suframa
+        registro_0000.IND_PERFIL = 'A'  # perfil
+        registro_0000.IND_ATIV = '1'  # tipo_atividade
         return registro_0000
 
-<<<<<<< HEAD
-    # def query_registro0100(self):
-    #     query = """
-    #
-    #     """
-    #     self._cr.execute(query)
-    #     query_resposta = self._cr.fetchall()
-    #
-    #     registro_0100 = registros.Registro0100()
-    #     registro_0100.NOME = query_resposta[0][0]
-    #     cpnj_cpf = self.limpa_formatacao(query_resposta[0][1])
-    #     if cpnj_cpf == 11:
-    #         registro_0100.CPF = cpnj_cpf
-    #     else:
-    #         registro_0100.CNPJ = cpnj_cpf
-    #     registro_0100.CRC = query_resposta[0][10]
-    #     registro_0100.CEP = self.limpa_formatacao(query_resposta[0][2])
-    #     registro_0100.END = query_resposta[0][3]
-    #     registro_0100.NUM = query_resposta[0][4]
-    #     registro_0100.COMPL = query_resposta[0][5]
-    #     registro_0100.BAIRRO = query_resposta[0][6]
-    #     registro_0100.FONE = self.limpa_formatacao(query_resposta[0][7])
-    #     registro_0100.EMAIL = query_resposta[0][8]
-    #     registro_0100.COD_MUN = self.formata_cod_municipio(query_resposta[0][9])
-    #
-    #     return registro_0100
-=======
     def query_registro0100(self):
         query = """
                     select 
@@ -160,10 +126,9 @@ class SpedEFD(models.Model):
         registro_0100.COD_MUN = self.formata_cod_municipio(resposta_municipio.codigo_ibge)
 
         return registro_0100
->>>>>>> a2fff02... [WIP] fluxo de validacao
 
     def query_registro0005(self):
-        query= """
+        query = """
              select
                  p.id
              from
@@ -193,18 +158,6 @@ class SpedEFD(models.Model):
 
     def query_registro0150(self):
         query = """
-<<<<<<< HEAD
-                    select distinct 
-                    p.nome, p.cnpj_cpf, p.ie, m.codigo_ibge, p.suframa, 
-                    p.endereco, p.numero, p.complemento, p.bairro
-                    from 
-                    sped_participante as p 
-                    join sped_municipio as m on p.municipio_id=m.id
-                """
-
-        self._cr.execute(query)
-        query_resposta = self._cr.fetchall()
-=======
                     select distinct
                         par.id
                     from
@@ -241,26 +194,8 @@ class SpedEFD(models.Model):
             registro_0150.COMPL = resposta_participante.complemento
             registro_0150.BAIRRO = resposta_participante.bairro
             lista.append(registro_0150)
->>>>>>> a2fff02... [WIP] fluxo de validacao
 
-        registro_0150 = registros.Registro0150()
-        registro_0150.COD_PART = '1' # TODO: arrumar a query_resposta
-        registro_0150.NOME = query_resposta[0][0]
-        registro_0150.COD_PAIS = '1058' # TODO: arrumar a query_resposta
-        cpnj_cpf = self.limpa_formatacao(query_resposta[0][1])
-        if len(cpnj_cpf) == 11:
-            registro_0150.CPF = cpnj_cpf
-        else:
-            registro_0150.CNPJ = cpnj_cpf
-        registro_0150.IE = self.limpa_formatacao(query_resposta[0][2])
-        registro_0150.COD_MUN = self.formata_cod_municipio(query_resposta[0][3])
-        registro_0150.SUFRAMA = self.limpa_formatacao(query_resposta[0][4])
-        registro_0150.END = query_resposta[0][5]
-        registro_0150.NUM = query_resposta[0][6]
-        registro_0150.COMPL = query_resposta[0][7]
-        registro_0150.BAIRRO = query_resposta[0][8]
-
-        return registro_0150
+        return lista
 
     def query_registro0190(self):
         query = """
@@ -282,7 +217,6 @@ class SpedEFD(models.Model):
             registro_0190.UNID = resposta.codigo_unico
             registro_0190.DESCR = resposta.nome_unico
             lista.append(registro_0190)
-
         return lista
 
     def query_registro0400(self):
@@ -292,13 +226,9 @@ class SpedEFD(models.Model):
                     from
                         sped_documento as d
                         join sped_natureza_operacao as no on d.natureza_operacao_id=no.id
-<<<<<<< HEAD
-                """
-=======
                     where
                         d.data_entrada_saida>='%s' and data_entrada_saida<='%s' and e.company_id='%s'
                 """ % (self.dt_ini[:10], self.dt_fim[:10], self.company_id.id)
->>>>>>> a2fff02... [WIP] fluxo de validacao
         self._cr.execute(query)
         query_resposta = self._cr.fetchall()
         lista = []
@@ -307,7 +237,6 @@ class SpedEFD(models.Model):
             registro_0400.COD_NAT = resposta[0]
             registro_0400.DESCR_NAT = resposta[1]
             lista.append(registro_0400)
-
         return lista
 
     def query_registro0200(self):
@@ -327,55 +256,12 @@ class SpedEFD(models.Model):
 
         self._cr.execute(query)
         query_resposta = self._cr.fetchall()
-<<<<<<< HEAD
-=======
         hash = {}
->>>>>>> a2fff02... [WIP] fluxo de validacao
         lista = []
+        cont = 1
         for resposta in query_resposta:
-            cont = 0
             resposta_produto = self.env['sped.produto'].browse(resposta[0])
             resposta_unidade = self.env['sped.unidade'].browse(resposta[1])
-<<<<<<< HEAD
-            resposta_item = self.env['sped.documento.item'].browse(resposta[2])
-            resposta_cfop = self.env['sped.cfop'].browse(resposta[3])
-            registro_0200 = registros.Registro0200()
-            registro_0200.COD_ITEM = resposta_produto.codigo_unico
-            registro_0200.DESCR_ITEM = resposta_produto.nome
-            registro_0200.COD_BARRA =  resposta_produto.codigo_barras
-            # registro_0200.COD_ANT_ITEM = query_resposta[0]
-            registro_0200.UNID_INV = resposta_unidade.codigo_unico
-            registro_0200.TIPO_ITEM =  resposta_produto.tipo
-            # registro_0200.COD_NCM = query_resposta[0]
-            # registro_0200.EX_IPI = query_resposta[0]
-            # registro_0200.COD_GEN = query_resposta[0]
-            # registro_0200.COD_LST = query_resposta[0]
-            # registro_0200.ALIQ_ICMS = query_resposta[0]
-            # registro_h005 = registros.RegistroH005()
-            # registro_h005.DT_INV = '02022017'
-            # registro_h005.VL_INV = '12'
-            # registro_h005.MOT_INV = '01'
-            lista.append(self.query_registro_C170(cont, resposta_item, resposta_produto,
-                                                  resposta_unidade, resposta_cfop))
-            cont += 1
-            lista.append(registro_0200)
-        return lista
-
-    def transforma_valor(self, valor):
-        return str(valor).replace('.',',')
-
-    def query_registro_C170(self, cont, documento_item, registro_0200,
-                            resposta_unidade, resposta_cfop):
-        registro_c170 = registros.RegistroC170()
-        registro_c170.NUM_ITEM = cont
-        registro_c170.COD_ITEM = registro_0200.codigo_unico
-        registro_c170.QTD = str(int(documento_item.quantidade))
-        registro_c170.UNID = resposta_unidade.codigo_unico
-        registro_c170.VL_ITEM = self.transforma_valor(str(int(documento_item.vr_unitario)*float(documento_item.vr_produtos)))
-        registro_c170.IND_MOV = '0'
-        registro_c170.CST_ICMS = documento_item.cst_icms_sn
-        registro_c170.CFOP = str(resposta_cfop.codigo)
-=======
             if not (resposta_produto.codigo_unico in hash):
                 registro_0200 = registros.Registro0200()
                 registro_0200.COD_ITEM = resposta_produto.codigo_unico
@@ -448,18 +334,12 @@ class SpedEFD(models.Model):
             registro_c170.ALIQ_ICMS = '0' # TODO: analisar com sadamo
         registro_c170.CFOP = resposta_documento.cfop_id.codigo
 
->>>>>>> a2fff02... [WIP] fluxo de validacao
         return registro_c170
 
     def query_registro_C100(self):
         query = """
-<<<<<<< HEAD
-                    select distinct 
-                        d.id, i.id
-=======
                     select distinct
                         d.id, par.id, di.id, p.id
->>>>>>> a2fff02... [WIP] fluxo de validacao
                     from
                         sped_documento as d
                         join sped_documento_item as di on d.id=di.documento_id
@@ -478,17 +358,6 @@ class SpedEFD(models.Model):
         cont = 1
         for id in query_resposta:
             resposta = self.env['sped.documento'].browse(id[0])
-<<<<<<< HEAD
-            resposta_item = self.env['sped.documento.item'].browse(id[1])
-            registro_c100 = registros.RegistroC100()
-            registro_c100.IND_OPER = resposta.entrada_saida
-            registro_c100.IND_EMIT = resposta.emissao
-            registro_c100.COD_PART = '1'
-            registro_c100.COD_MOD = resposta.modelo
-            registro_c100.COD_SIT = resposta.situacao_fiscal
-
-            if resposta.modelo == MODELO_FISCAL_NFE or resposta.modelo == MODELO_FISCAL_NFCE:
-=======
             resposta_participante = self.env['sped.participante'].browse(id[1])
             resposta_item = self.env['sped.documento.item'].browse(id[2])
             resposta_produto = self.env['sped.produto'].browse(id[3])
@@ -499,7 +368,6 @@ class SpedEFD(models.Model):
                 registro_c100.COD_PART = str(resposta_participante.id)
                 registro_c100.COD_MOD = resposta.modelo
                 registro_c100.COD_SIT = resposta.situacao_fiscal
->>>>>>> a2fff02... [WIP] fluxo de validacao
                 registro_c100.SER = resposta.serie
                 registro_c100.CHV_NFE = resposta.chave
                 registro_c100.NUM_DOC = self.limpa_formatacao(str(int(resposta.numero)))
@@ -540,64 +408,6 @@ class SpedEFD(models.Model):
 
         return lista
 
-<<<<<<< HEAD
-    # def query_registro_D100(self):
-    #     query = """
-    #         select distinct
-    #             d.id
-    #         from
-    #             sped_documento as d
-    #             join sped_empresa as e on e.id = d.empresa_id
-    #             join sped_participante as p on p.id = e.participante_id
-    #         where
-    #             d.data_entrada_saida>='%s' and data_entrada_saida<='%s'
-    #         """ % (self.dt_ini[:10], self.dt_fim[:10])
-    #     self._cr.execute(query)
-    #     query_resposta = self._cr.fetchall()
-    #     lista = []
-    #     for id in query_resposta:
-    #         resposta = self.env['sped.documento'].browse(id[0])
-    #         registro_d100 = registros.RegistroD100()
-    #         registro_d100.IND_OPER = resposta.entrada_saida
-    #         registro_d100.IND_EMIT = resposta.emissao
-    #         registro_d100.COD_PART = '1'
-    #         registro_d100.COD_MOD = resposta.modelo
-    #         registro_d100.COD_SIT = resposta.situacao_fiscal
-    #
-    #             registro_d100.SER = resposta.serie
-    #         # registro_d100.SUB
-    #
-    #         registro_d100.NUM_DOC = self.limpa_formatacao(str(resposta.numero))
-    #         # registro_d100.CHV_CTE
-    #         registro_d100.DT_DOC = self.transforma_data(resposta.data_entrada_saida)
-    #         # registro_d100.DT_A_P
-    #         # registro_d100.TP_CT-e
-    #         # registro_d100.CHV_CTE_REF
-    #         registro_d100.VL_DOC = self.limpa_formatacao(str(resposta.vr_produtos))
-    #         # registro_d100.VL_DESC
-    #         # registro_d100.IND_FRT
-    #         registro_d100.VL_SERV = '10' # TODO: achar valor do servico
-    #         # registro_d100.VL_BC_ICMS
-    #         # registro_d100.VL_ICMS
-    #         # registro_d100.VL_NT
-    #         # registro_d100.COD_INF
-    #         # registro_d100.COD_CTA
-    #         lista.append(registro_d100)
-    #
-    #     return lista
-
-    def query_registro_H005(self):
-        query = """
-        
-        """
-        self._cr.execute(query)
-        query_resposta = self._cr.fetchall()
-        lista = []
-        for id in query_resposta:
-            resposta = self.env['sped.documento'].browse(id[0])
-
-=======
->>>>>>> a2fff02... [WIP] fluxo de validacao
     def query_registro_E100(self):
         registro_E100 = registros.RegistroE100()
         registro_E100.DT_INI = self.transforma_data(self.dt_ini[:10])
@@ -638,32 +448,6 @@ class SpedEFD(models.Model):
         # arq.read_registro('|9900|9999|1|')
         # cont_9900 = 2
         # bloco 0
-<<<<<<< HEAD
-        lista_c170 = []
-        arq.read_registro(self.junta_pipe(self.query_registro0000()))
-        # arq.read_registro(self.junta_pipe(self.query_registro0100()))
-        arq.read_registro(self.junta_pipe(self.query_registro0005()))
-        arq.read_registro(self.junta_pipe(self.query_registro0150()))
-        for item_lista in self.query_registro0190():
-            arq.read_registro(self.junta_pipe(item_lista))
-        for item_lista in self.query_registro0200():
-            if isinstance(item_lista, registros.RegistroC170):
-               lista_c170.append(item_lista)
-            else:
-                arq.read_registro(self.junta_pipe(item_lista))
-        for item_lista in self.query_registro0400():
-            arq.read_registro(self.junta_pipe(item_lista))
-
-        # bloco C
-        for item_lista in self.query_registro_C100():
-            arq.read_registro(self.junta_pipe(item_lista))
-        for item_lista in lista_c170:
-            arq.read_registro(self.junta_pipe(item_lista))
-
-        # bloco D
-        # for item_lista in self.query_registro_D100():
-        #     arq.read_registro(self.junta_pipe(item_lista))
-=======
         lista_c190 = []
         # bloco 0
         arq.read_registro(self.junta_pipe(self.query_registro0000()))
@@ -685,7 +469,6 @@ class SpedEFD(models.Model):
             arq.read_registro(item_lista)
         for item_lista in lista_c190:
             arq.read_registro(item_lista)
->>>>>>> a2fff02... [WIP] fluxo de validacao
 
         # bloco E
         arq.read_registro(self.junta_pipe(self.query_registro_E100()))
@@ -696,11 +479,11 @@ class SpedEFD(models.Model):
 
         #bloco 9
         for bloco in arq._blocos.items():
-                for registros_bloco in bloco[1].registros:
-                    if registros_bloco._valores[1] in hash:
-                        hash[registros_bloco._valores[1]] = int(hash[registros_bloco._valores[1]]) + 1
-                    else:
-                        hash[registros_bloco._valores[1]] = 1
+            for registros_bloco in bloco[1].registros:
+                if registros_bloco._valores[1] in hash:
+                    hash[registros_bloco._valores[1]] = int(hash[registros_bloco._valores[1]]) + 1
+                else:
+                    hash[registros_bloco._valores[1]] = 1
 
         for key, value in hash.items():
             registro_9900 = registros.Registro9900()
@@ -710,11 +493,10 @@ class SpedEFD(models.Model):
         #
         registro_9900 = registros.Registro9900()
         registro_9900.REG_BLC = '9900'
-        registro_9900.QTD_REG_BLC = str(len(hash)+1)
+        registro_9900.QTD_REG_BLC = str(len(hash) + 1)
         arq.read_registro(self.junta_pipe(registro_9900))
 
         arquivo = self.env['ir.attachment']
-
 
         dados = {
             'name': 'teste.txt',
@@ -722,12 +504,6 @@ class SpedEFD(models.Model):
             'res_model': 'sped.efd',
             'res_id': self.id,
             'datas': base64.b64encode(arq.getstring().encode('utf-8')),
-            'mimetype':'application/txt'
+            'mimetype': 'application/txt'
         }
         arquivo.create(dados)
-
-
-
-
-
-

23771c64c2e8d45462e246b8e3404ee8fd450068
Atilla Silva <atillaa4@hotmail.com>
2018-01-11 18:09:55 -0200

    [ADD] versao final da migracao do sped_participante

--- l10n_br_base/models/base_participante.py
+++ l10n_br_base/models/base_participante.py
@@ -92,6 +92,7 @@ class BaseParticipante(SpedBase):
     eh_vendedor = fields.Boolean(
         string='É vendedor/representante?'
     )
+
     cnpj_cpf = fields.Char(
         string='CNPJ/CPF',
         size=18,
@@ -272,6 +273,11 @@ class BaseParticipante(SpedBase):
     codigo_ans = fields.Char(
         comodel_name='Código ANS',
         size=6
+    )
+    #  euro campo
+    fone_whatsapp = fields.Char(
+        string='WhatsApp',
+        size=18
     )
     #
     # Para a NFC-e, ECF, SAT
@@ -943,3 +949,39 @@ class BaseParticipante(SpedBase):
             #valores['user_id'] = False
 
         #return res
+        # update
+        # res_partner
+        # set
+        # (fone, profissao, celular, eh_funcionario, complemento,
+        #  eh_orgao_publico, eh_grupo, eh_consumidor_final, contribuinte,
+        #  suframa, cei, codigo_ans, cep, rg_data_expedicao, municipio_id,
+        #  eh_usuario, rg_orgao_emissor, eh_cliente, eh_convenio, crc,
+        #  email_nfe, create_date, eh_cooperativa, codigo_sindical,
+        #  razao_social, email, regime_tributario, rntrc, tipo_pessoa, cidade,
+        #  eh_empresa, write_date, eh_transportadora, create_uid,
+        #  transportadora_id, nome, message_last_post, cnpj_cpf, eh_fornecedor,
+        #  fone_comercial, codigo, site, im, rg_numero, ie,
+        #  bairro, numero, eh_sindicato, estado, fantasia, crc_uf,
+        #  pais_nacionalidade_id, endereco, date_localization,
+        #  partner_latitude, partner_longitude, condicao_pagamento_id,
+        #  eh_vendedor,
+        #  cnpj_cpf_raiz, fone_whatsapp, cnpj_cpf_numero) = (
+        # p.fone, p.profissao, p.celular, p.eh_funcionario,
+        # p.complemento, p.eh_orgao_publico, p.eh_grupo, p.eh_consumidor_final,
+        # p.contribuinte, p.suframa, p.cei, p.codigo_ans, p.cep,
+        # p.rg_data_expedicao, p.municipio_id, p.eh_usuario, p.rg_orgao_emissor,
+        # p.eh_cliente, p.eh_convenio, p.crc, p.email_nfe, p.create_date,
+        # p.eh_cooperativa, p.codigo_sindical, p.razao_social,
+        # p.email, p.regime_tributario, p.rntrc, p.tipo_pessoa, p.cidade,
+        # p.eh_empresa, p.write_date, p.eh_transportadora, p.create_uid,
+        # p.transportadora_id, p.nome, p.message_last_post, p.cnpj_cpf,
+        # p.eh_fornecedor, p.fone_comercial, p.codigo,
+        # p.site, p.im, p.rg_numero, p.ie, p.bairro, p.numero, p.eh_sindicato,
+        # p.estado, p.fantasia, p.crc_uf, p.pais_nacionalidade_id, p.endereco,
+        # p.date_localization, p.partner_latitude,p.partner_longitude,
+        # p.condicao_pagamento_id, p.eh_vendedor, p.cnpj_cpf_raiz,
+        # p.fone_whatsapp, p.cnpj_cpf_numero)
+        # from sped_participante p
+        # where
+        # res_partner.id = p.partner_id
+
--- l10n_br_base/models/inherited_res_partner.py
+++ l10n_br_base/models/inherited_res_partner.py
@@ -12,6 +12,8 @@ from odoo import api, fields, models
 
 class ResPartner(BaseParticipante, models.Model):
     _inherit = 'res.partner'
+    _rec_name = 'nome'
+    _order = 'nome, cnpj_cpf'
 
     sped_participante_id = fields.Many2one(
         comodel_name='sped.participante',

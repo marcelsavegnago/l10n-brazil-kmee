a9a1eafc3d90f589e6527d1c7e2aee81248d0c00
Luiz Felipe do Divino <lf.divino@gmail.com>
2018-01-11 11:45:07 -0300

    [FIX] Adicionado a loja na configuração do PDV

--- sped_cfe/models/pdv_config.py
+++ sped_cfe/models/pdv_config.py
@@ -15,6 +15,11 @@ class ConfiguracaoPDV(models.Model):
         string=u'Vendedor',
     )
 
+    loja = fields.Many2one(
+        comodel_name='sped.empresa',
+        string=u'Loja',
+    )
+
     numero_caixa = fields.Char(string=u'Número de caixa')
 
     tipo = fields.Selection([('SAT', 'SAT'),

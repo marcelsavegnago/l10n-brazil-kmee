f5d204596025e2119e8271e48583eaa2c42b953b
Daniel Sadamo Hirayama <daniel.sadamo@kmee.com.br>
2018-01-19 20:19:27 -0200

    Correções antigas que estavam unstaged

--- l10n_br_account_service/models/account_invoice.py
+++ l10n_br_account_service/models/account_invoice.py
@@ -89,8 +89,9 @@ class AccountInvoice(models.Model):
         nfse_tree = ('l10n_br_account_service.'
                      'l10n_br_account_service_nfse_tree')
         nfse_views = {'form': nfse_form, 'tree': nfse_tree}
-
-        if active_id:
+	
+	fiscal_document_code = False
+        if active_id and context.get('active_model') == 'account.invoice':
             invoice = self.browse(active_id)
             fiscal_document_code = invoice.fiscal_document_id.code
 

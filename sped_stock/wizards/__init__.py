f192a15c08cd013e0c04235d07d495f850b1e527
Luis Felipe Mileo <mileo@kmee.com.br>
2018-02-19 07:51:20 -0300

    [REF] Criação do estoque a partir do documento fiscal

--- sped_stock/wizards/__init__.py
+++ sped_stock/wizards/__init__.py
@@ -2,9 +2,11 @@
 #
 # Copyright 2017 Taŭga Tecnologia
 #    Aristides Caldeira <aristides.caldeira@tauga.com.br>
+# Copyright 2018 KMEE INFORMATICA LTDA
 # License AGPL-3 or later (http://www.gnu.org/licenses/agpl)
 #
 
 
 from . import inherited_make_procurement
 from . import inherited_change_product_quantity
+from . import inherited_sped_documento_devolucao

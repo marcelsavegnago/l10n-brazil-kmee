b0108cb6ec3111561c0fe7af223f0bf71beb6f47
Luis Felipe Mileo <mileo@kmee.com.br>
2018-03-21 19:28:33 -0300

    fixup! [REM] Duplicidade de código

--- sped/__manifest__.py
+++ sped/__manifest__.py
@@ -63,8 +63,6 @@
         # Dados de base
         #
         'data/sped_participante_data.xml',
-        'wizard/sped_documento_exportar_xml.xml',
-        'wizard/base_config_settings.xml',
     ],
     'external_dependencies': {
         'python': [

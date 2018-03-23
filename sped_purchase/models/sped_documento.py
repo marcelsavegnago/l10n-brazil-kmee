49cd4a983b6a07f4bdee9274740d1ca25cb5071c
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2018-03-23 16:05:13 -0300

    [ADD] Funcionalidades do account_invoice

--- sped_purchase/models/sped_documento.py
+++ sped_purchase/models/sped_documento.py
@@ -17,10 +17,17 @@ class SpedDocumento(models.Model):
     )
 
     def _preparar_sped_documento_item(self, item):
+        quantidade = 0.0
+        if self.stock_picking_id:
+            quantidades = self.stock_picking_id.mapped('move_lines').filtered(
+                lambda move: move.purchase_line_id.id == item.id).mapped(
+                'product_qty')
+            for qty in quantidades:
+                quantidade += qty
         dados = {
             'product_id': item.product_id.id,
             'documento_id': self.id,
-            'quantidade': item.quantidade - item.qty_invoiced,
+            'quantidade': quantidade or (item.quantidade - item.qty_invoiced),
             'vr_unitario': item.vr_unitario,
             'vr_frete': item.vr_frete,
             'vr_seguro': item.vr_seguro,
@@ -34,80 +41,53 @@ class SpedDocumento(models.Model):
     def purchase_order_change(self):
         if not self.purchase_id:
             return {}
-        elif len(self.purchase_order_ids) == 1:
-            dados = {
-                'empresa_id': self.purchase_order_ids.empresa_id.id,
-                'operacao_id': self.purchase_order_ids.operacao_id.id,
-                'modelo': self.purchase_order_ids.operacao_id.modelo,
-                'emissao': self.purchase_order_ids.operacao_id.emissao,
-                # 'participante_id': self.purchase_order_ids.participante_id.id,
-                'condicao_pagamento_id': self.purchase_order_ids.condicao_pagamento_id.id if \
-                    self.purchase_order_ids.condicao_pagamento_id else False,
-                'transportadora_id': self.purchase_order_ids.transportadora_id.id if \
-                    self.purchase_order_ids.transportadora_id else False,
-                'modalidade_frete': self.purchase_order_ids.modalidade_frete,
-            }
-            dados.update(self.purchase_order_ids.prepara_dados_documento())
-            self.update(dados)
-            self.update(self._onchange_empresa_id()['value'])
-            self.update(self._onchange_operacao_id()['value'])
-    
-            if self.purchase_order_ids.presenca_comprador:
-                self.presenca_comprador = self.purchase_order_ids.presenca_comprador
-    
-            self.update(self._onchange_serie()['value'])
-            # self.update(self._onchange_participante_id()['value'])
+        pick = self.purchase_id.picking_ids.filtered(
+            lambda pick: pick.state != 'cancel'
+        ) - self.purchase_id.invoice_ids.mapped('stock_picking_id')
+        pick = pick.filtered(lambda pick: pick.state == 'done') or pick
+        dados = {
+            'empresa_id': self.purchase_id.empresa_id.id,
+            'operacao_id': self.purchase_id.operacao_id.id,
+            'modelo': self.purchase_id.operacao_id.modelo,
+            'emissao': self.purchase_id.operacao_id.emissao,
+            'partner_id': self.purchase_id.partner_id.id,
+            'condicao_pagamento_id':
+                self.purchase_id.condicao_pagamento_id.id or False,
+            'natureza_operacao_id':
+                self.purchase_id.operacao_id.natureza_operacao_id.id or False,
+            'serie': self.purchase_id.operacao_id.serie or False,
+            'presenca_comprador':
+                self.purchase_id.presenca_comprador or False,
+            'stock_picking_id': pick and pick[0].id or False,
+        }
+        dados.update(self._onchange_empresa_id()['value'])
+        dados.update(self._onchange_operacao_id()['value'])
+        dados.update(self._onchange_serie()['value'])
+        self.update(dados)
 
-        for pedido in self.mapped('purchase_order_ids'):
-            for item in pedido.order_line - \
-                    self.item_ids.mapped('purchase_line_ids'):
-                dados = self._preparar_sped_documento_item(item)
-                contexto = {
-                    'forca_vr_unitario': dados['vr_unitario']
-                }
-                documento_item = self.item_ids.mesclar_linhas(
-                    dados, item, contexto
-                )
-                if documento_item:
-                    self.item_ids += documento_item
-                    self.item_ids[-1].update(
-                        item.prepara_dados_documento_item()
-                    )
+        novos_itens = self.env['sped.documento.item']
+        for item in self.purchase_id.order_line - \
+                self.item_ids.mapped('purchase_line_id'):
+            dados = self._preparar_sped_documento_item(item)
+            dados.update(
+                item.prepara_dados_documento_item()
+            )
+            documento_item = novos_itens.new(dados)
+            documento_item.calcula_impostos()
+            novos_itens += documento_item
+
+        self.item_ids |= novos_itens
+        self.purchase_id = False
+        self.importado_xml = True
         return {}
 
-    @api.onchange('partner_id', 'item_ids')
-    def _onchange_allowed_purchase_ids(self):
-        result = {}
-
-    @api.multi
-    def write(self, vals):
-        res = super(SpedDocumento, self).write(vals)
-        purchase_order_ids = self.mapped('purchase_order_ids')
-        for pedido in purchase_order_ids:
-            pedido._get_invoiced()
-        return res
-
-    @api.onchange('purchase_order_ids')
-    def purchase_order_change(self):
-        self.ensure_one()
-        if len(self.mapped('purchase_order_ids')) > 1:
+    @api.onchange('sped_stock_id')
+    def stock_picking_change(self):
+        if not self.stock_picking_id:
             return {}
-        elif not self.purchase_order_ids:
-            res = {'value': {'item_ids': [(1, item.id, {
-                'purchase_ids': [(6, 0, [])],
-                'purchase_line_ids': [(6, 0, [])]
-            }) for item in self.mapped('item_ids')]}}
-            return res
-        for item in self.mapped('item_ids'):
-            item.purchase_ids = self.purchase_order_ids.ids
-        self.item_ids.find_lines()
-
-    def _criar_picking_entrada(self):
-        if not self.purchase_order_ids:
-            super(SpedDocumento, self)._criar_picking_entrada()
-
-    def executa_depois_create(self, result, dados):
-        for documento in result:
-            for item in documento.item_ids:
-                item.calcula_impostos()
-        return result
+        dados = {
+            'transportadora_id':
+                self.stock_picking_id.transportadora_id.id or False,
+            'modalidade_frete':
+                self.stock_picking_id.modalidade_frete or False,
+        }

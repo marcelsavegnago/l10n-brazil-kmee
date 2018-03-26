8f5dacdafd359c84a41bfd5180f02e6d5f7200a5
Hugo Borges <hugo.borges@kmee.com.br>
2018-03-26 11:58:46 -0300

    [IMP] Movendo sped_consulta_status_documento para o Sped_NFe

--- sped_purchase/__manifest__.py
+++ sped_purchase/__manifest__.py
@@ -25,7 +25,6 @@
         'views/sped_documento_emissao_nfe_view.xml',
         'views/sped_documento_item.xml',
         'views/stock_picking.xml',
-        'wizard/sped_consulta_status_documento.xml',
     ],
     'demo': [
         # 'demo/purchase_order_line.xml',

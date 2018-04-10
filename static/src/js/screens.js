4eb29f9e3a871b7472f572c66d0ebe5ccbc8b7a3
Atilla Silva <atilla.silva@kmee.com.br>
2018-04-10 18:53:33 -0300

    [FIX] bloqueio retirado ao selecionar outro cliente pelo popup

--- static/src/js/screens.js
+++ static/src/js/screens.js
@@ -617,6 +617,8 @@ function l10n_br_pos_screens(instance, module) {
                     self.pos_widget.screen_selector.close_popup();
                 }
                 if (!currentOrder.client) {
+                    $(".pos-leftpane *").prop('disabled', save_state);
+                    self.pos_widget.screen_selector.close_popup();
                     if (self.pos_widget.order_widget.verificar_cpf_cnpj(cpf.replace(/[^\d]+/g,''))) {
                         $(".pos-leftpane *").prop('disabled', save_state);
                         self.pos_widget.screen_selector.close_popup();
@@ -689,6 +691,8 @@ function l10n_br_pos_screens(instance, module) {
                         if (partner[key] === 'false' )
                             partner[key] = null;
                     }
+                    $(".pos-leftpane *").prop('disabled', save_state);
+                    self.pos_widget.screen_selector.close_popup();
                     currentOrder = self.pos.get('selectedOrder').attributes;
                     currentOrder["cpf_nota"] = cpf.replace(/[^\d]+/g,'');
                     // this.active_client(self, documento, partner);

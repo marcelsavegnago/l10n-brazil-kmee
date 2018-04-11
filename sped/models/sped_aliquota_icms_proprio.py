OCA/l10n-brazil

Remoção de códigos não compatíveis com o odoo/10.0 e formatação de estilo de código

sped/models/sped_aliquota_icms_proprio.py
@@ -5,7 +5,7 @@
 #
 
 
-from __future__ import division, print_function, unicode_literals
+
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -23,43 +23,65 @@ from ..constante_tributaria import *
 
 
 class AliquotaICMSProprio(models.Model):
-    _description = 'Alíquota do ICMS próprio'
+    _description = u'Alíquota do ICMS próprio'
     _name = 'sped.aliquota.icms.proprio'
     _rec_name = 'descricao'
     _order = 'al_icms, md_icms, pr_icms, rd_icms'
 
-    al_icms = fields.Float('Alíquota', required=True, digits=(5, 2))
-    md_icms = fields.Selection(MODALIDADE_BASE_ICMS_PROPRIO, 'Modalidade da base de cálculo', required=True,
-                               default=MODALIDADE_BASE_ICMS_PROPRIO_VALOR_OPERACAO)
-    pr_icms = fields.Float('Parâmetro da base de cálculo', digits=(18, 4),
-                                help='A margem de valor agregado, ou o valor da pauta/preço tabelado máximo, '
-                                     'de acordo com o definido na modalidade da base de cálculo.')
-    rd_icms = fields.Float('Percentual de redução da alíquota', digits=(5, 2))
-    importado = fields.Boolean('Padrão para produtos importados?', default=False)
-    descricao = fields.Char(string='Alíquota do ICMS próprio', compute='_compute_descricao', store=False)
+    al_icms = fields.Float(
+        string=u'Alíquota',
+        required=True,
+        digits=(5, 2)
+    )
+    md_icms = fields.Selection(
+        selection=MODALIDADE_BASE_ICMS_PROPRIO,
+        string=u'Modalidade da base de cálculo',
+        required=True,
+        default=MODALIDADE_BASE_ICMS_PROPRIO_VALOR_OPERACAO
+    )
+    pr_icms = fields.Float(
+        string=u'Parâmetro da base de cálculo',
+        digits=(18, 4),
+        help=u'A margem de valor agregado, ou o valor da pauta/preço tabelado '
+             u'máximo, de acordo com o definido na modalidade da base de '
+             u'cálculo.',
+    )
+    rd_icms = fields.Float(
+        string=u'Percentual de redução da alíquota',
+        digits=(5, 2),
+    )
+    importado = fields.Boolean(
+        string=u'Padrão para produtos importados?',
+        default=False
+    )
+    descricao = fields.Char(
+        string=u'Alíquota do ICMS próprio',
+        compute='_compute_descricao',
+        store=False
+    )
 
     @api.depends('al_icms', 'md_icms', 'pr_icms', 'rd_icms', 'importado')
     def _compute_descricao(self):
         for al_icms in self:
             if al_icms.al_icms == -1:
-                al_icms.descricao = 'Não tributado'
+                al_icms.descricao = u'Não tributado'
             else:
                 al_icms.descricao = formata_valor(al_icms.al_icms or 0) + '%'
 
                 if al_icms.md_icms != MODALIDADE_BASE_ICMS_PROPRIO_VALOR_OPERACAO:
                     if al_icms.md_icms == MODALIDADE_BASE_ICMS_PROPRIO_MARGEM_VALOR_AGREGADO:
-                        al_icms.descricao += ', por MVA de ' + formata_valor(al_icms.pr_icms, casas_decimais=4) + '%'
+                        al_icms.descricao += u', por MVA de ' + formata_valor(al_icms.pr_icms, casas_decimais=4) + '%'
                     elif al_icms.md_icms == MODALIDADE_BASE_ICMS_PROPRIO_PAUTA:
-                        al_icms.descricao += ', por pauta de R$ ' + formata_valor(al_icms.pr_icms, casas_decimais=4)
+                        al_icms.descricao += u', por pauta de R$ ' + formata_valor(al_icms.pr_icms, casas_decimais=4)
                     elif al_icms.md_icms == MODALIDADE_BASE_ICMS_PROPRIO_PRECO_TABELADO_MAXIMO:
-                        al_icms.descricao += ', por preço máximo de R$ ' + \
+                        al_icms.descricao += u', por preço máximo de R$ ' + \
                                              formata_valor(al_icms.pr_icms, casas_decimais=4)
 
                 if al_icms.rd_icms != 0:
-                    al_icms.descricao += ', com redução de ' + formata_valor(al_icms.rd_icms) + '%'
+                    al_icms.descricao += u', com redução de ' + formata_valor(al_icms.rd_icms) + '%'
 
                 if al_icms.importado:
-                    al_icms.descricao += ' (padrão para importados)'
+                    al_icms.descricao += u' (padrão para importados)'
 
     @api.depends('al_icms', 'md_icms', 'pr_icms', 'rd_icms')
     def _check_al_icms(self):
@@ -77,4 +99,4 @@ class AliquotaICMSProprio(models.Model):
             al_icms_ids = self.search(busca)
 
             if al_icms_ids:
-                raise ValidationError('Alíquota de ICMS já existe!')
+                raise ValidationError(u'Alíquota de ICMS já existe!')

sped/models/sped_aliquota_icms_st.py
@@ -5,7 +5,7 @@
 #
 
 
-from __future__ import division, print_function, unicode_literals
+
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -23,53 +23,76 @@ from ..constante_tributaria import *
 
 
 class AliquotaICMSST(models.Model):
-    _description = 'Alíquota do ICMS ST'
+    _description = u'Alíquota do ICMS ST'
     _name = 'sped.aliquota.icms.st'
     _rec_name = 'descricao'
     _order = 'al_icms, md_icms, pr_icms, rd_icms, rd_mva'
 
-    al_icms = fields.Float('Alíquota', required=True, digits=(5, 2))
-    md_icms = fields.Selection(MODALIDADE_BASE_ICMS_ST, 'Modalidade da base de cálculo', required=True,
-                               default=MODALIDADE_BASE_ICMS_ST_MARGEM_VALOR_AGREGADO)
-    pr_icms = fields.Float('Parâmetro da base de cálculo', required=True, digits=(18, 4),
-                                help='A margem de valor agregado, ou o valor da pauta/preço tabelado máximo/lista, '
-                                     'de acordo com o definido na modalidade da base de cálculo.')
-    rd_icms = fields.Float('Percentual de redução da alíquota', digits=(5, 2))
-    rd_mva = fields.Float('Percentual de redução do MVA para o SIMPLES', digits=(5, 2))
-    descricao = fields.Char(string='Alíquota do ICMS ST', compute='_compute_descricao', store=False)
+    al_icms = fields.Float(
+        string=u'Alíquota',
+        required=True,
+        digits=(5, 2),
+    )
+    md_icms = fields.Selection(
+        selection=MODALIDADE_BASE_ICMS_ST,
+        string=u'Modalidade da base de cálculo',
+        required=True,
+        default=MODALIDADE_BASE_ICMS_ST_MARGEM_VALOR_AGREGADO
+    )
+    pr_icms = fields.Float(
+        string=u'Parâmetro da base de cálculo',
+        required=True,
+        digits=(18, 4),
+        help=u'A margem de valor agregado, ou o valor da pauta/preço tabelado '
+             u'máximo/lista, de acordo com o definido na modalidade da '
+             u'base de cálculo.'
+    )
+    rd_icms = fields.Float(
+        string=u'Percentual de redução da alíquota',
+        digits=(5, 2)
+    )
+    rd_mva = fields.Float(
+        string=u'Percentual de redução do MVA para o SIMPLES',
+        digits=(5, 2)
+    )
+    descricao = fields.Char(
+        string=u'Alíquota do ICMS ST',
+        compute='_compute_descricao',
+        store=False
+    )
 
     @api.depends('al_icms', 'md_icms', 'pr_icms', 'rd_icms')
     def _compute_descricao(self):
         for al_icms in self:
             if al_icms.al_icms == -1:
-                al_icms.descricao = 'Não tributado'
+                al_icms.descricao = u'Não tributado'
             else:
                 al_icms.descricao = formata_valor(al_icms.al_icms or 0) + '%'
 
                 if al_icms.md_icms == MODALIDADE_BASE_ICMS_ST_PRECO_TABELADO_MAXIMO:
-                    al_icms.descricao += ', por preço máximo'
+                    al_icms.descricao += u', por preço máximo'
                 elif al_icms.md_icms == MODALIDADE_BASE_ICMS_ST_LISTA_NEGATIVA:
-                    al_icms.descricao += ', por lista negativa'
+                    al_icms.descricao += u', por lista negativa'
                 elif al_icms.md_icms == MODALIDADE_BASE_ICMS_ST_LISTA_POSITIVA:
-                    al_icms.descricao += ', por lista positiva'
+                    al_icms.descricao += u', por lista positiva'
                 elif al_icms.md_icms == MODALIDADE_BASE_ICMS_ST_LISTA_NEUTRA:
-                    al_icms.descricao += ', por lista neutra'
+                    al_icms.descricao += u', por lista neutra'
                 elif al_icms.md_icms == MODALIDADE_BASE_ICMS_ST_MARGEM_VALOR_AGREGADO:
-                    al_icms.descricao += ', por MVA'
+                    al_icms.descricao += u', por MVA'
                 elif al_icms.md_icms == MODALIDADE_BASE_ICMS_ST_PAUTA:
-                    al_icms.descricao += ', por pauta'
+                    al_icms.descricao += u', por pauta'
 
                 if al_icms.pr_icms:
                     if al_icms.md_icms == MODALIDADE_BASE_ICMS_ST_MARGEM_VALOR_AGREGADO:
-                        al_icms.descricao += ' de ' + formata_valor(al_icms.pr_icms, casas_decimais=4) + '%'
+                        al_icms.descricao += u' de ' + formata_valor(al_icms.pr_icms, casas_decimais=4) + '%'
                     else:
-                        al_icms.descricao += ' de R$ ' + formata_valor(al_icms.pr_icms, casas_decimais=4)
+                        al_icms.descricao += u' de R$ ' + formata_valor(al_icms.pr_icms, casas_decimais=4)
 
                 if al_icms.rd_icms != 0:
-                    al_icms.descricao += ', com redução de ' + formata_valor(al_icms.rd_icms) + '%'
+                    al_icms.descricao += u', com redução de ' + formata_valor(al_icms.rd_icms) + '%'
 
                 if al_icms.rd_mva != 0:
-                    al_icms.descricao += ', com MVA reduzido em ' + formata_valor(al_icms.rd_mva) + '% para o SIMPLES'
+                    al_icms.descricao += u', com MVA reduzido em ' + formata_valor(al_icms.rd_mva) + '% para o SIMPLES'
 
     @api.depends('al_icms', 'md_icms', 'pr_icms', 'rd_icms', 'rd_mva')
     def _check_al_icms(self):
@@ -88,4 +111,4 @@ class AliquotaICMSST(models.Model):
             al_icms_ids = self.search(busca)
 
             if al_icms_ids:
-                raise ValidationError('Alíquota de ICMS ST já existe!')
+                raise ValidationError(u'Alíquota de ICMS ST já existe!')

marcelsavegnago/l10n-brazil-kmee

sped/models/sped_aliquota_icms_proprio.py
@@ -5,7 +5,7 @@
 #
 
 
-from __future__ import division, print_function, unicode_literals
+
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -23,44 +23,68 @@ from ..constante_tributaria import *
 
 
 class AliquotaICMSProprio(models.Model):
-    _description = 'Alíquota do ICMS próprio'
+    _description = u'Alíquota do ICMS próprio'
     _inherit = 'sped.base'
     _name = 'sped.aliquota.icms.proprio'
     _rec_name = 'descricao'
     _order = 'al_icms, md_icms, pr_icms, rd_icms'
 
-    al_icms = fields.Monetary('Alíquota', required=True, digits=(5, 2), currency_field='currency_aliquota_id')
-    md_icms = fields.Selection(MODALIDADE_BASE_ICMS_PROPRIO, 'Modalidade da base de cálculo', required=True,
-                               default=MODALIDADE_BASE_ICMS_PROPRIO_VALOR_OPERACAO)
-    pr_icms = fields.Float('Parâmetro da base de cálculo', digits=(18, 4),
-                                help='A margem de valor agregado, ou o valor da pauta/preço tabelado máximo, '
-                                     'de acordo com o definido na modalidade da base de cálculo.')
-    rd_icms = fields.Monetary('Percentual de redução da alíquota', digits=(5, 2), currency_field='currency_aliquota_id')
-    importado = fields.Boolean('Padrão para produtos importados?', default=False)
-    descricao = fields.Char(string='Alíquota do ICMS próprio', compute='_compute_descricao', store=False)
+    al_icms = fields.Monetary(
+        string=u'Alíquota',
+        required=True,
+        digits=(5, 2),
+	currency_field='currency_aliquota_id'
+    )
+    md_icms = fields.Selection(
+        selection=MODALIDADE_BASE_ICMS_PROPRIO,
+        string=u'Modalidade da base de cálculo',
+        required=True,
+        default=MODALIDADE_BASE_ICMS_PROPRIO_VALOR_OPERACAO
+    )
+    pr_icms = fields.Float(
+        string=u'Parâmetro da base de cálculo',
+        digits=(18, 4),
+        help=u'A margem de valor agregado, ou o valor da pauta/preço tabelado '
+             u'máximo, de acordo com o definido na modalidade da base de '
+             u'cálculo.',
+    )
+    rd_icms = fields.Monetary(
+        string=u'Percentual de redução da alíquota',
+        digits=(5, 2),
+        currency_field='currency_aliquota_id'
+    )
+    importado = fields.Boolean(
+        string=u'Padrão para produtos importados?',
+        default=False
+    )
+    descricao = fields.Char(
+        string=u'Alíquota do ICMS próprio',
+        compute='_compute_descricao',
+        store=False
+    )
 
     @api.depends('al_icms', 'md_icms', 'pr_icms', 'rd_icms', 'importado')
     def _compute_descricao(self):
         for al_icms in self:
             if al_icms.al_icms == -1:
-                al_icms.descricao = 'Não tributado'
+                al_icms.descricao = u'Não tributado'
             else:
                 al_icms.descricao = formata_valor(al_icms.al_icms or 0) + '%'
 
                 if al_icms.md_icms != MODALIDADE_BASE_ICMS_PROPRIO_VALOR_OPERACAO:
                     if al_icms.md_icms == MODALIDADE_BASE_ICMS_PROPRIO_MARGEM_VALOR_AGREGADO:
-                        al_icms.descricao += ', por MVA de ' + formata_valor(al_icms.pr_icms, casas_decimais=4) + '%'
+                        al_icms.descricao += u', por MVA de ' + formata_valor(al_icms.pr_icms, casas_decimais=4) + '%'
                     elif al_icms.md_icms == MODALIDADE_BASE_ICMS_PROPRIO_PAUTA:
-                        al_icms.descricao += ', por pauta de R$ ' + formata_valor(al_icms.pr_icms, casas_decimais=4)
+                        al_icms.descricao += u', por pauta de R$ ' + formata_valor(al_icms.pr_icms, casas_decimais=4)
                     elif al_icms.md_icms == MODALIDADE_BASE_ICMS_PROPRIO_PRECO_TABELADO_MAXIMO:
-                        al_icms.descricao += ', por preço máximo de R$ ' + \
+                        al_icms.descricao += u', por preço máximo de R$ ' + \
                                              formata_valor(al_icms.pr_icms, casas_decimais=4)
 
                 if al_icms.rd_icms != 0:
-                    al_icms.descricao += ', com redução de ' + formata_valor(al_icms.rd_icms) + '%'
+                    al_icms.descricao += u', com redução de ' + formata_valor(al_icms.rd_icms) + '%'
 
                 if al_icms.importado:
-                    al_icms.descricao += ' (padrão para importados)'
+                    al_icms.descricao += u' (padrão para importados)'
 
     @api.depends('al_icms', 'md_icms', 'pr_icms', 'rd_icms')
     def _check_al_icms(self):
@@ -78,4 +102,4 @@ class AliquotaICMSProprio(models.Model):
             al_icms_ids = self.search(busca)
 
             if al_icms_ids:
-                raise ValidationError('Alíquota de ICMS já existe!')
+                raise ValidationError(u'Alíquota de ICMS já existe!')

sped/models/sped_aliquota_icms_st.py
@@ -5,7 +5,7 @@
 #
 
 
-from __future__ import division, print_function, unicode_literals
+
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -23,54 +23,80 @@ from ..constante_tributaria import *
 
 
 class AliquotaICMSST(models.Model):
-    _description = 'Alíquota do ICMS ST'
+    _description = u'Alíquota do ICMS ST'
     _inherit = 'sped.base'
     _name = 'sped.aliquota.icms.st'
     _rec_name = 'descricao'
     _order = 'al_icms, md_icms, pr_icms, rd_icms, rd_mva'
 
-    al_icms = fields.Monetary('Alíquota', required=True, digits=(5, 2), currency_field='currency_aliquota_id')
-    md_icms = fields.Selection(MODALIDADE_BASE_ICMS_ST, 'Modalidade da base de cálculo', required=True,
-                               default=MODALIDADE_BASE_ICMS_ST_MARGEM_VALOR_AGREGADO)
-    pr_icms = fields.Float('Parâmetro da base de cálculo', required=True, digits=(18, 4),
-                                help='A margem de valor agregado, ou o valor da pauta/preço tabelado máximo/lista, '
-                                     'de acordo com o definido na modalidade da base de cálculo.')
-    rd_icms = fields.Monetary('Percentual de redução da alíquota', digits=(5, 2), currency_field='currency_aliquota_id')
-    rd_mva = fields.Monetary('Percentual de redução do MVA para o SIMPLES', digits=(5, 2), currency_field='currency_aliquota_id')
-    descricao = fields.Char(string='Alíquota do ICMS ST', compute='_compute_descricao', store=False)
+    al_icms = fields.Monetary(
+        string=u'Alíquota',
+        required=True,
+        digits=(5, 2),
+        currency_field='currency_aliquota_id',
+    )
+    md_icms = fields.Selection(
+        selection=MODALIDADE_BASE_ICMS_ST,
+        string=u'Modalidade da base de cálculo',
+        required=True,
+        default=MODALIDADE_BASE_ICMS_ST_MARGEM_VALOR_AGREGADO
+    )
+    pr_icms = fields.Float(
+        string=u'Parâmetro da base de cálculo',
+        required=True,
+        digits=(18, 4),
+        help=u'A margem de valor agregado, ou o valor da pauta/preço tabelado '
+             u'máximo/lista, de acordo com o definido na modalidade da '
+             u'base de cálculo.'
+    )
+    rd_icms = fields.Monetary(
+        string=u'Percentual de redução da alíquota',
+        digits=(5, 2),
+        currency_field='currency_aliquota_id',
+    )
+    rd_mva = fields.Monetary(
+        string=u'Percentual de redução do MVA para o SIMPLES',
+        digits=(5, 2),
+        currency_field='currency_aliquota_id',
+    )
+    descricao = fields.Char(
+        string=u'Alíquota do ICMS ST',
+        compute='_compute_descricao',
+        store=False
+    )
 
     @api.depends('al_icms', 'md_icms', 'pr_icms', 'rd_icms')
     def _compute_descricao(self):
         for al_icms in self:
             if al_icms.al_icms == -1:
-                al_icms.descricao = 'Não tributado'
+                al_icms.descricao = u'Não tributado'
             else:
                 al_icms.descricao = formata_valor(al_icms.al_icms or 0) + '%'
 
                 if al_icms.md_icms == MODALIDADE_BASE_ICMS_ST_PRECO_TABELADO_MAXIMO:
-                    al_icms.descricao += ', por preço máximo'
+                    al_icms.descricao += u', por preço máximo'
                 elif al_icms.md_icms == MODALIDADE_BASE_ICMS_ST_LISTA_NEGATIVA:
-                    al_icms.descricao += ', por lista negativa'
+                    al_icms.descricao += u', por lista negativa'
                 elif al_icms.md_icms == MODALIDADE_BASE_ICMS_ST_LISTA_POSITIVA:
-                    al_icms.descricao += ', por lista positiva'
+                    al_icms.descricao += u', por lista positiva'
                 elif al_icms.md_icms == MODALIDADE_BASE_ICMS_ST_LISTA_NEUTRA:
-                    al_icms.descricao += ', por lista neutra'
+                    al_icms.descricao += u', por lista neutra'
                 elif al_icms.md_icms == MODALIDADE_BASE_ICMS_ST_MARGEM_VALOR_AGREGADO:
-                    al_icms.descricao += ', por MVA'
+                    al_icms.descricao += u', por MVA'
                 elif al_icms.md_icms == MODALIDADE_BASE_ICMS_ST_PAUTA:
-                    al_icms.descricao += ', por pauta'
+                    al_icms.descricao += u', por pauta'
 
                 if al_icms.pr_icms:
                     if al_icms.md_icms == MODALIDADE_BASE_ICMS_ST_MARGEM_VALOR_AGREGADO:
-                        al_icms.descricao += ' de ' + formata_valor(al_icms.pr_icms, casas_decimais=4) + '%'
+                        al_icms.descricao += u' de ' + formata_valor(al_icms.pr_icms, casas_decimais=4) + '%'
                     else:
-                        al_icms.descricao += ' de R$ ' + formata_valor(al_icms.pr_icms, casas_decimais=4)
+                        al_icms.descricao += u' de R$ ' + formata_valor(al_icms.pr_icms, casas_decimais=4)
 
                 if al_icms.rd_icms != 0:
-                    al_icms.descricao += ', com redução de ' + formata_valor(al_icms.rd_icms) + '%'
+                    al_icms.descricao += u', com redução de ' + formata_valor(al_icms.rd_icms) + '%'
 
                 if al_icms.rd_mva != 0:
-                    al_icms.descricao += ', com MVA reduzido em ' + formata_valor(al_icms.rd_mva) + '% para o SIMPLES'
+                    al_icms.descricao += u', com MVA reduzido em ' + formata_valor(al_icms.rd_mva) + '% para o SIMPLES'
 
     @api.depends('al_icms', 'md_icms', 'pr_icms', 'rd_icms', 'rd_mva')
     def _check_al_icms(self):
@@ -89,4 +115,4 @@ class AliquotaICMSST(models.Model):
             al_icms_ids = self.search(busca)
 
             if al_icms_ids:
-                raise ValidationError('Alíquota de ICMS ST já existe!')
+                raise ValidationError(u'Alíquota de ICMS ST já existe!')
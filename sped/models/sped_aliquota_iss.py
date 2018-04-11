OCA/l10n-brazil

Remoção de códigos não compatíveis com o odoo/10.0 e formatação de estilo de código

sped/models/sped_aliquota_iss.py
@@ -5,19 +5,36 @@
 #
 
 
-from __future__ import division, print_function, unicode_literals
+
 
 from odoo import fields, models
 import odoo.addons.decimal_precision as dp
 
 
 class AliquotaISS(models.Model):
-    _description = 'Alíquota do ISS'
+    _description = u'Alíquota do ISS'
     _name = 'sped.aliquota.iss'
     _rec_name = 'al_iss'
     _order = 'servico_id, municipio_id, al_iss'
 
-    servico_id = fields.Many2one('sped.servico', 'Serviço', ondelete='cascade', required=True)
-    municipio_id = fields.Many2one('sped.municipio', 'Município', ondelete='restrict', required=True)
-    al_iss = fields.Float('Alíquota', required=True, digits=(5, 2))
-    codigo = fields.Char('Código específico', size=10)
+    servico_id = fields.Many2one(
+        'sped.servico',
+        string=u'Serviço',
+        ondelete='cascade',
+        required=True,
+    )
+    municipio_id = fields.Many2one(
+        comodel_name='sped.municipio',
+        string=u'Município',
+        ondelete='restrict',
+        required=True
+    )
+    al_iss = fields.Float(
+        string=u'Alíquota',
+        required=True,
+        digits=(5, 2),
+    )
+    codigo = fields.Char(
+        string=u'Código específico',
+        size=10,
+    )

sped/models/sped_aliquota_pis_cofins.py
@@ -5,7 +5,7 @@
 #
 
 
-from __future__ import division, print_function, unicode_literals
+
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -17,52 +17,80 @@ except (ImportError, IOError) as err:
     _logger.debug(err)
 
 from odoo import api, fields, models
-import odoo.addons.decimal_precision as dp
 from odoo.exceptions import ValidationError
 from ..constante_tributaria import *
 
 
 class AliquotaPISCOFINS(models.Model):
-    _description = 'Alíquota do PIS-COFINS'
+    _description = u'Alíquota do PIS-COFINS'
     _name = 'sped.aliquota.pis.cofins'
     _rec_name = 'descricao'
     _order = 'al_pis, al_cofins'
 
-    al_pis = fields.Float('Alíquota do PIS', required=True, digits=(5, 2))
-    al_cofins = fields.Float('Alíquota da COFINS', required=True, digits=(5, 2))
-    md_pis_cofins = fields.Selection(MODALIDADE_BASE_PIS, 'Modalidade da base de cálculo', required=True,
-                                     default=MODALIDADE_BASE_PIS_ALIQUOTA)
-    cst_pis_cofins_entrada = fields.Selection(ST_PIS_ENTRADA, 'Situação tributária nas entradas', required=True,
-                                              default=ST_PIS_CRED_EXCL_TRIB_MERC_INTERNO)
-    cst_pis_cofins_saida = fields.Selection(ST_PIS_SAIDA, 'Situação tributária nas saída', required=True,
-                                            default=ST_PIS_TRIB_NORMAL)
-    codigo_justificativa = fields.Char('Código da justificativa', size=10)
-    descricao = fields.Char(string='Alíquota do PIS-COFINS', compute='_compute_descricao', store=True)
+    al_pis = fields.Float(
+        string=u'Alíquota do PIS',
+        required=True,
+        digits=(5, 2),
+    )
+    al_cofins = fields.Float(
+        string=u'Alíquota da COFINS',
+        required=True,
+        digits=(5, 2),
+    )
+    md_pis_cofins = fields.Selection(
+        selection=MODALIDADE_BASE_PIS,
+        string=u'Modalidade da base de cálculo',
+        required=True,
+        default=MODALIDADE_BASE_PIS_ALIQUOTA,
+    )
+    cst_pis_cofins_entrada = fields.Selection(
+        selection=ST_PIS_ENTRADA,
+        string=u'Situação tributária nas entradas',
+        required=True,
+        default=ST_PIS_CRED_EXCL_TRIB_MERC_INTERNO,
+    )
+    cst_pis_cofins_saida = fields.Selection(
+        selection=ST_PIS_SAIDA,
+        string=u'Situação tributária nas saída',
+        required=True,
+        default=ST_PIS_TRIB_NORMAL,
+    )
+    codigo_justificativa = fields.Char(
+        string=u'Código da justificativa',
+        size=10,
+    )
+    descricao = fields.Char(
+        string=u'Alíquota do PIS-COFINS',
+        compute='_compute_descricao',
+        store=True,
+    )
 
-    @api.depends('al_pis', 'al_cofins', 'md_pis_cofins', 'cst_pis_cofins_entrada', 'cst_pis_cofins_saida',
+    @api.depends('al_pis', 'al_cofins', 'md_pis_cofins',
+                 'cst_pis_cofins_entrada', 'cst_pis_cofins_saida',
                  'codigo_justificativa')
     def _compute_descricao(self):
         for al_pis_cofins in self:
             if al_pis_cofins.al_pis == -1:
-                al_pis_cofins.descricao = 'Não tributado'
+                al_pis_cofins.descricao = u'Não tributado'
             else:
                 if al_pis_cofins.md_pis_cofins == MODALIDADE_BASE_PIS_ALIQUOTA:
-                    al_pis_cofins.descricao = 'PIS ' + formata_valor(al_pis_cofins.al_pis or 0) + '%'
-                    al_pis_cofins.descricao += '; COFINS ' + formata_valor(al_pis_cofins.al_cofins or 0) + '%'
+                    al_pis_cofins.descricao = u'PIS ' + formata_valor(al_pis_cofins.al_pis or 0) + '%'
+                    al_pis_cofins.descricao += u'; COFINS ' + formata_valor(al_pis_cofins.al_cofins or 0) + '%'
 
                 elif al_pis_cofins.md_pis_cofins == MODALIDADE_BASE_PIS_QUANTIDADE:
-                    al_pis_cofins.descricao = 'por quantidade, PIS a R$ ' + formata_valor(al_pis_cofins.al_pis)
-                    al_pis_cofins.descricao += '; COFINS a R$ ' + formata_valor(al_pis_cofins.al_cofins)
+                    al_pis_cofins.descricao = u'por quantidade, PIS a R$ ' + formata_valor(al_pis_cofins.al_pis)
+                    al_pis_cofins.descricao += u'; COFINS a R$ ' + formata_valor(al_pis_cofins.al_cofins)
 
-                al_pis_cofins.descricao += ' - CST ' + al_pis_cofins.cst_pis_cofins_entrada
-                al_pis_cofins.descricao += ' entrada, ' + al_pis_cofins.cst_pis_cofins_saida
-                al_pis_cofins.descricao += ' saída'
+                al_pis_cofins.descricao += u' - CST ' + al_pis_cofins.cst_pis_cofins_entrada
+                al_pis_cofins.descricao += u' entrada, ' + al_pis_cofins.cst_pis_cofins_saida
+                al_pis_cofins.descricao += u' saída'
 
                 if al_pis_cofins.codigo_justificativa:
-                    al_pis_cofins.descricao += ' - justificativa '
+                    al_pis_cofins.descricao += u' - justificativa '
                     al_pis_cofins.descricao += al_pis_cofins.codigo_justificativa
 
-    @api.depends('al_pis', 'al_cofins', 'md_pis_cofins', 'cst_pis_cofins_entrada', 'cst_pis_cofins_saida',
+    @api.depends('al_pis', 'al_cofins', 'md_pis_cofins',
+                 'cst_pis_cofins_entrada', 'cst_pis_cofins_saida',
                  'codigo_justificativa')
     def _check_al_pis(self):
         for al_pis_cofins in self:
@@ -81,4 +109,4 @@ class AliquotaPISCOFINS(models.Model):
             al_pis_cofins_ids = self.search(busca)
 
             if al_pis_cofins_ids:
-                raise ValidationError('Alíquotas de PIS e COFINS já existem!')
+                raise ValidationError(u'Alíquotas de PIS e COFINS já existem!')

sped/models/sped_aliquota_simples.py
@@ -5,7 +5,7 @@
 #
 
 
-from __future__ import division, print_function, unicode_literals
+
 
 from odoo import api, fields, models
 import odoo.addons.decimal_precision as dp
@@ -13,30 +13,39 @@ from odoo.exceptions import ValidationError
 
 
 class AliquotaSIMPLESAnexo(models.Model):
-    _description = 'Anexo do SIMPLES Nacional'
+    _description = u'Anexo do SIMPLES Nacional'
     _name = 'sped.aliquota.simples.anexo'
     _rec_name = 'nome'
     _order = 'nome'
 
-    nome = fields.Char('Anexo do SIMPLES Nacional', size=40, index=True)
-    aliquota_ids = fields.One2many('sped.aliquota.simples.aliquota', 'anexo_id', 'Alíquotas')
+    nome = fields.Char(
+        string=u'Anexo do SIMPLES Nacional',
+        size=40,
+        index=True
+    )
+    aliquota_ids = fields.One2many(
+        comodel_name='sped.aliquota.simples.aliquota',
+        inverse_name='anexo_id',
+        string=u'Alíquotas'
+    )
 
     @api.depends('nome')
     def _check_nome(self):
         for anexo in self:
             if anexo.id:
-                anexo_ids = self.search([('nome', '=', anexo.nome), ('id', '!=', anexo.id)])
+                anexo_ids = self.search(
+                    [('nome', '=', anexo.nome), ('id', '!=', anexo.id)])
             else:
                 anexo_ids = self.search([('nome', '=', anexo.nome)])
 
             if anexo_ids:
-                raise ValidationError('Anexo já existe na tabela!')
+                raise ValidationError(u'Anexo já existe na tabela!')
 
         return res
 
 
 class AliquotaSIMPLESTeto(models.Model):
-    _description = 'Teto do SIMPLES Nacional'
+    _description = u'Teto do SIMPLES Nacional'
     _name = 'sped.aliquota.simples.teto'
     _rec_name = 'nome'
     _order = 'valor'
@@ -45,18 +54,31 @@ class AliquotaSIMPLESTeto(models.Model):
     # Para todos os valores de teto de alíquota do SIMPLES, a moeda é sempre o
     # Real BRL
     #
-    currency_id = fields.Many2one('res.currency', 'Moeda', default=lambda self: self.env.ref('base.BRL').id)
-
-    valor = fields.Monetary('Valor do teto do SIMPLES Nacional', required=True, index=True)
-    nome = fields.Char('Teto do SIMPLES Nacional', size=40, index=True)
+    currency_id = fields.Many2one(
+        comodel_name='res.currency',
+        string=u'Moeda',
+        default=lambda self: self.env.ref('base.BRL').id,
+    )
+    valor = fields.Monetary(
+        string=u'Valor do teto do SIMPLES Nacional',
+        required=True,
+        index=True,
+    )
+    nome = fields.Char(
+        string=u'Teto do SIMPLES Nacional',
+        size=40,
+        index=True,
+    )
 
     @api.depends('valor')
     def _check_valor(self):
         for teto in self:
             if teto.id:
-                teto_ids = self.search([('valor', '=', teto.valor), ('id', '!=', teto.id)])
+                teto_ids = self.search(
+                    [('valor', '=', teto.valor), ('id', '!=', teto.id)])
             else:
-                teto_ids = self.search([('valor', '=', teto.valor)])
+                teto_ids = self.search(
+                    [('valor', '=', teto.valor)])
 
             if teto_ids:
                 raise ValidationError('Teto já existe na tabela!')
@@ -65,18 +87,52 @@ class AliquotaSIMPLESTeto(models.Model):
 
 
 class AliquotaSIMPLESAliquota(models.Model):
-    _description = 'Alíquota do SIMPLES Nacional'
+    _description = u'Alíquota do SIMPLES Nacional'
     _name = 'sped.aliquota.simples.aliquota'
     _rec_name = 'al_simples'
     _order = 'anexo_id, teto_id'
 
-    anexo_id = fields.Many2one('sped.aliquota.simples.anexo', 'Anexo', required=True, ondelete='cascade')
-    teto_id = fields.Many2one('sped.aliquota.simples.teto', 'Teto', required=True, ondelete='cascade')
-    al_simples = fields.Float('SIMPLES', digits=(5, 2))
-    al_irpj = fields.Float('IRPJ', digits=(5, 2))
-    al_csll = fields.Float('CSLL', digits=(5, 2))
-    al_cofins = fields.Float('COFINS', digits=(5, 2))
-    al_pis = fields.Float('PIS', digits=(5, 2))
-    al_cpp = fields.Float('CPP', digits=(5, 2))
-    al_icms = fields.Float('ICMS', digits=(5, 2))
-    al_iss = fields.Float('ISS', digits=(5, 2))
+    anexo_id = fields.Many2one(
+        comodel_name='sped.aliquota.simples.anexo',
+        string=u'Anexo',
+        required=True,
+        ondelete='cascade',
+    )
+    teto_id = fields.Many2one(
+        comodel_name='sped.aliquota.simples.teto',
+        string=u'Teto',
+        required=True,
+        ondelete='cascade',
+    )
+    al_simples = fields.Float(
+        string=u'SIMPLES',
+        digits=(5, 2),
+    )
+    al_irpj = fields.Float(
+        string=u'IRPJ',
+        digits=(5, 2),
+    )
+    al_csll = fields.Float(
+        string=u'CSLL',
+        digits=(5, 2),
+    )
+    al_cofins = fields.Float(
+        string=u'COFINS',
+        digits=(5, 2),
+    )
+    al_pis = fields.Float(
+        string=u'PIS',
+        digits=(5, 2)
+    )
+    al_cpp = fields.Float(
+        string=u'CPP',
+        digits=(5, 2)
+    )
+    al_icms = fields.Float(
+        string=u'ICMS',
+        digits=(5, 2)
+    )
+    al_iss = fields.Float(
+        string=u'ISS',
+        digits=(5, 2)
+    )

marcelsavegnago/l10n-brazil-kmee

sped/models/sped_aliquota_iss.py
@@ -10,13 +10,31 @@ import odoo.addons.decimal_precision as dp
 
 
 class AliquotaISS(models.Model):
-    _description = 'Alíquota do ISS'
+    _description = u'Alíquota do ISS'
     _inherit = 'sped.base'
     _name = 'sped.aliquota.iss'
     _rec_name = 'al_iss'
     _order = 'servico_id, municipio_id, al_iss'
 
-    servico_id = fields.Many2one('sped.servico', 'Serviço', ondelete='cascade', required=True)
-    municipio_id = fields.Many2one('sped.municipio', 'Município', ondelete='restrict', required=True)
-    al_iss = fields.Monetary('Alíquota', required=True, digits=(5, 2), currency_field='currency_aliquota_id')
-    codigo = fields.Char('Código específico', size=10)
+    servico_id = fields.Many2one(
+        'sped.servico',
+        string=u'Serviço',
+        ondelete='cascade',
+        required=True,
+    )
+    municipio_id = fields.Many2one(
+        comodel_name='sped.municipio',
+        string=u'Município',
+        ondelete='restrict',
+        required=True,
+    )
+    al_iss = fields.Monetary(
+        string=u'Alíquota',
+        required=True,
+        digits=(5, 2),
+        currency_field='currency_aliquota_id',
+    )
+    codigo = fields.Char(
+        string=u'Código específico',
+        size=10,
+    )

sped/models/sped_aliquota_pis_cofins.py
@@ -20,24 +20,54 @@ from ..constante_tributaria import *
 
 
 class AliquotaPISCOFINS(models.Model):
-    _description = 'Alíquota do PIS-COFINS'
+    _description = u'Alíquota do PIS-COFINS'
     _inherit = 'sped.base'
     _name = 'sped.aliquota.pis.cofins'
     _rec_name = 'descricao'
     _order = 'al_pis, al_cofins'
 
-    al_pis = fields.Monetary('Alíquota do PIS', required=True, digits=(5, 2), currency_field='currency_aliquota_id')
-    al_cofins = fields.Monetary('Alíquota da COFINS', required=True, digits=(5, 2), currency_field='currency_aliquota_id')
-    md_pis_cofins = fields.Selection(MODALIDADE_BASE_PIS, 'Modalidade da base de cálculo', required=True,
-                                     default=MODALIDADE_BASE_PIS_ALIQUOTA)
-    cst_pis_cofins_entrada = fields.Selection(ST_PIS_ENTRADA, 'Situação tributária nas entradas', required=True,
-                                              default=ST_PIS_CRED_EXCL_TRIB_MERC_INTERNO)
-    cst_pis_cofins_saida = fields.Selection(ST_PIS_SAIDA, 'Situação tributária nas saída', required=True,
-                                            default=ST_PIS_TRIB_NORMAL)
-    codigo_justificativa = fields.Char('Código da justificativa', size=10)
-    descricao = fields.Char(string='Alíquota do PIS-COFINS', compute='_compute_descricao', store=True)
+    al_pis = fields.Monetary(
+        string=u'Alíquota do PIS',
+        required=True,
+        digits=(5, 2),
+        currency_field='currency_aliquota_id',
+    )
+    al_cofins = fields.Monetary(
+        string=u'Alíquota da COFINS',
+        required=True,
+        digits=(5, 2),
+        currency_field='currency_aliquota_id',
+    )
+    md_pis_cofins = fields.Selection(
+        selection=MODALIDADE_BASE_PIS,
+        string=u'Modalidade da base de cálculo',
+        required=True,
+        default=MODALIDADE_BASE_PIS_ALIQUOTA,
+    )
+    cst_pis_cofins_entrada = fields.Selection(
+        selection=ST_PIS_ENTRADA,
+        string=u'Situação tributária nas entradas',
+        required=True,
+        default=ST_PIS_CRED_EXCL_TRIB_MERC_INTERNO,
+    )
+    cst_pis_cofins_saida = fields.Selection(
+        selection=ST_PIS_SAIDA,
+        string=u'Situação tributária nas saída',
+        required=True,
+        default=ST_PIS_TRIB_NORMAL,
+    )
+    codigo_justificativa = fields.Char(
+        string=u'Código da justificativa',
+        size=10,
+    )
+    descricao = fields.Char(
+        string=u'Alíquota do PIS-COFINS',
+        compute='_compute_descricao',
+        store=True,
+    )
 
-    @api.depends('al_pis', 'al_cofins', 'md_pis_cofins', 'cst_pis_cofins_entrada', 'cst_pis_cofins_saida',
+    @api.depends('al_pis', 'al_cofins', 'md_pis_cofins',
+                 'cst_pis_cofins_entrada', 'cst_pis_cofins_saida',
                  'codigo_justificativa')
     def _compute_descricao(self):
         for al_pis_cofins in self:
@@ -45,16 +75,12 @@ class AliquotaPISCOFINS(models.Model):
                 al_pis_cofins.descricao = u'Não tributado'
             else:
                 if al_pis_cofins.md_pis_cofins == MODALIDADE_BASE_PIS_ALIQUOTA:
-                    al_pis_cofins.descricao = u'PIS ' + \
-                        formata_valor(al_pis_cofins.al_pis or 0) + '%'
-                    al_pis_cofins.descricao += u'; COFINS ' + \
-                        formata_valor(al_pis_cofins.al_cofins or 0) + '%'
+                    al_pis_cofins.descricao = u'PIS ' + formata_valor(al_pis_cofins.al_pis or 0) + '%'
+                    al_pis_cofins.descricao += u'; COFINS ' + formata_valor(al_pis_cofins.al_cofins or 0) + '%'
 
                 elif al_pis_cofins.md_pis_cofins == MODALIDADE_BASE_PIS_QUANTIDADE:
-                    al_pis_cofins.descricao = u'por quantidade, PIS a R$ ' + \
-                        formata_valor(al_pis_cofins.al_pis)
-                    al_pis_cofins.descricao += u'; COFINS a R$ ' + \
-                        formata_valor(al_pis_cofins.al_cofins)
+                    al_pis_cofins.descricao = u'por quantidade, PIS a R$ ' + formata_valor(al_pis_cofins.al_pis)
+                    al_pis_cofins.descricao += u'; COFINS a R$ ' + formata_valor(al_pis_cofins.al_cofins)
 
                 al_pis_cofins.descricao += u' - CST ' + al_pis_cofins.cst_pis_cofins_entrada
                 al_pis_cofins.descricao += u' entrada, ' + al_pis_cofins.cst_pis_cofins_saida

sped/models/sped_aliquota_simples.py
@@ -43,14 +43,22 @@ class AliquotaSIMPLESAnexo(models.Model):
 
 
 class AliquotaSIMPLESTeto(models.Model):
-    _description = 'Teto do SIMPLES Nacional'
+    _description = u'Teto do SIMPLES Nacional'
     _inherit = 'sped.base'
     _name = 'sped.aliquota.simples.teto'
     _rec_name = 'nome'
     _order = 'valor'
 
-    valor = fields.Monetary('Valor do teto do SIMPLES Nacional', required=True, index=True)
-    nome = fields.Char('Teto do SIMPLES Nacional', size=40, index=True)
+    valor = fields.Monetary(
+        string=u'Valor do teto do SIMPLES Nacional',
+        required=True,
+        index=True,
+    )
+    nome = fields.Char(
+        string=u'Teto do SIMPLES Nacional',
+        size=40,
+        index=True,
+    )
 
     @api.depends('valor')
     def _check_valor(self):
@@ -69,19 +77,61 @@ class AliquotaSIMPLESTeto(models.Model):
 
 
 class AliquotaSIMPLESAliquota(models.Model):
-    _description = 'Alíquota do SIMPLES Nacional'
+    _description = u'Alíquota do SIMPLES Nacional'
     _inherit = 'sped.base'
     _name = 'sped.aliquota.simples.aliquota'
     _rec_name = 'al_simples'
     _order = 'anexo_id, teto_id'
 
-    anexo_id = fields.Many2one('sped.aliquota.simples.anexo', 'Anexo', required=True, ondelete='cascade')
-    teto_id = fields.Many2one('sped.aliquota.simples.teto', 'Teto', required=True, ondelete='cascade')
-    al_simples = fields.Monetary('SIMPLES', digits=(5, 2), currency_field='currency_aliquota_id')
-    al_irpj = fields.Monetary('IRPJ', digits=(5, 2), currency_field='currency_aliquota_id')
-    al_csll = fields.Monetary('CSLL', digits=(5, 2), currency_field='currency_aliquota_id')
-    al_cofins = fields.Monetary('COFINS', digits=(5, 2), currency_field='currency_aliquota_id')
-    al_pis = fields.Monetary('PIS', digits=(5, 2), currency_field='currency_aliquota_id')
-    al_cpp = fields.Monetary('CPP', digits=(5, 2), currency_field='currency_aliquota_id')
-    al_icms = fields.Monetary('ICMS', digits=(5, 2), currency_field='currency_aliquota_id')
-    al_iss = fields.Monetary('ISS', digits=(5, 2), currency_field='currency_aliquota_id')
+    anexo_id = fields.Many2one(
+        comodel_name='sped.aliquota.simples.anexo',
+        string=u'Anexo',
+        required=True,
+        ondelete='cascade',
+    )
+    teto_id = fields.Many2one(
+        comodel_name='sped.aliquota.simples.teto',
+        string=u'Teto',
+        required=True,
+        ondelete='cascade',
+    )
+    al_simples = fields.Monetary(
+        string=u'SIMPLES',
+        digits=(5, 2),
+        currency_field='currency_aliquota_id',
+    )
+    al_irpj = fields.Monetary(
+        string=u'IRPJ',
+        digits=(5, 2),
+        currency_field='currency_aliquota_id',
+    )
+    al_csll = fields.Monetary(
+        string=u'CSLL',
+        digits=(5, 2),
+        currency_field='currency_aliquota_id',
+    )
+    al_cofins = fields.Monetary(
+        string=u'COFINS',
+        digits=(5, 2),
+        currency_field='currency_aliquota_id',
+    )
+    al_pis = fields.Monetary(
+        string=u'PIS',
+        digits=(5, 2),
+        currency_field='currency_aliquota_id',
+    )
+    al_cpp = fields.Monetary(
+        string=u'CPP',
+        digits=(5, 2),
+        currency_field='currency_aliquota_id',
+    )
+    al_icms = fields.Monetary(
+        string=u'ICMS',
+        digits=(5, 2),
+        currency_field='currency_aliquota_id',
+    )
+    al_iss = fields.Monetary(
+        string=u'ISS',
+        digits=(5, 2),
+        currency_field='currency_aliquota_id',
+    )
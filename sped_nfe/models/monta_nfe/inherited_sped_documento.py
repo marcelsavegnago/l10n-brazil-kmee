5f615b8e2c63e71749bba6a19cd96155cf5e98df
Luis Felipe Mileo <mileo@kmee.com.br>
2018-04-11 18:43:20 -0300

    [REF] Geração da nf-e

--- sped_nfe/models/monta_nfe/inherited_sped_documento.py
+++ sped_nfe/models/monta_nfe/inherited_sped_documento.py
@@ -151,7 +151,8 @@ class SpedDocumento(models.Model):
         ide.dSaiEnt.valor = ide.dhSaiEnt.valor
         ide.hSaiEnt.valor = ide.dhSaiEnt.valor
 
-        ide.cMunFG.valor = empresa.municipio_id.codigo_ibge[:7]
+        ide.cMunFG.valor = ('%s%s') % (
+            empresa.state_id.ibge_code, empresa.l10n_br_city_id.ibge_code)
         # ide.tpImp.valor = 1  # DANFE em retrato
         ide.tpEmis.valor = self.tipo_emissao_nfe
         ide.finNFe.valor = self.finalidade_nfe
@@ -184,20 +185,21 @@ class SpedDocumento(models.Model):
         empresa = self.empresa_id
 
         emit.CNPJ.valor = limpa_formatacao(empresa.cnpj_cpf)
-        emit.xNome.valor = empresa.razao_social
+        emit.xNome.valor = empresa.legal_name
         emit.xFant.valor = empresa.fantasia or ''
-        emit.enderEmit.xLgr.valor = empresa.endereco
-        emit.enderEmit.nro.valor = empresa.numero
-        emit.enderEmit.xCpl.valor = empresa.complemento or ''
-        emit.enderEmit.xBairro.valor = empresa.bairro
-        emit.enderEmit.cMun.valor = empresa.municipio_id.codigo_ibge[:7]
-        emit.enderEmit.xMun.valor = empresa.cidade
+        emit.enderEmit.xLgr.valor = empresa.street
+        emit.enderEmit.nro.valor = empresa.number
+        emit.enderEmit.xCpl.valor = empresa.street2 or ''
+        emit.enderEmit.xBairro.valor = empresa.district
+        emit.enderEmit.cMun.valor = ('%s%s') % (
+            empresa.state_id.ibge_code, empresa.l10n_br_city_id.ibge_code)
+        emit.enderEmit.xMun.valor = empresa.city
         emit.enderEmit.UF.valor = empresa.estado
-        emit.enderEmit.CEP.valor = limpa_formatacao(empresa.cep)
+        emit.enderEmit.CEP.valor = limpa_formatacao(empresa.zip)
         # emit.enderEmit.cPais.valor = '1058'
         # emit.enderEmit.xPais.valor = 'Brasil'
-        emit.enderEmit.fone.valor = limpa_formatacao(empresa.fone or '')
-        emit.IE.valor = limpa_formatacao(empresa.ie or '')
+        emit.enderEmit.fone.valor = limpa_formatacao(empresa.phone or '')
+        emit.IE.valor = limpa_formatacao(empresa.inscr_est or '')
         #
         # Usa o regime tributário da NF e não da empresa, e trata o código
         # interno 3.1 para o lucro real, que na NF deve ir somente 3
@@ -223,7 +225,7 @@ class SpedDocumento(models.Model):
 
             if partner.contribuinte == \
                     INDICADOR_IE_DESTINATARIO_CONTRIBUINTE:
-                dest.IE.valor = limpa_formatacao(partner.ie or '')
+                dest.IE.valor = limpa_formatacao(partner.inscr_est or '')
 
         #
         # Trata a possibilidade de ausência do destinatário na NFC-e
@@ -249,39 +251,40 @@ class SpedDocumento(models.Model):
             #dest.xNome.valor = 'NF-E EMITIDA EM AMBIENTE DE HOMOLOGACAO - ' \
                                #'SEM VALOR FISCAL'
         #else:
-        dest.xNome.valor = partner.razao_social or ''
+        dest.xNome.valor = partner.legal_name or ''
 
         #
         # Para a NFC-e, o endereço do partner pode não ter sido
         # preenchido
         #
         dest.enderDest.xLgr.valor = partner.street or ''
-        dest.enderDest.nro.valor = partner.numero or ''
+        dest.enderDest.nro.valor = partner.number or ''
         dest.enderDest.xCpl.valor = partner.street2 or ''
-        dest.enderDest.xBairro.valor = partner.bairro or ''
+        dest.enderDest.xBairro.valor = partner.district or ''
 
         if not partner.cnpj_cpf.startswith('EX'):
-            dest.enderDest.CEP.valor = limpa_formatacao(partner.cep or '')
+            dest.enderDest.CEP.valor = limpa_formatacao(partner.zip or '')
         else:
             dest.enderDest.CEP.valor = '99999999'
 
         #
         # Pode haver cadastro de partner sem município para NFC-e
         #
-        if partner.municipio_id:
-            dest.enderDest.cMun.valor = \
-                partner.municipio_id.codigo_ibge[:7]
-            dest.enderDest.xMun.valor = partner.cidade
+        if partner.l10n_br_city_id:
+            dest.enderDest.cMun.valor =('%s%s') % (
+                partner.state_id.ibge_code,
+                partner.l10n_br_city_id.ibge_code)
+            dest.enderDest.xMun.valor = partner.city
             dest.enderDest.UF.valor = partner.estado
 
             if partner.cnpj_cpf.startswith('EX'):
                 dest.enderDest.cPais.valor = \
-                    partner.municipio_id.pais_id.codigo_bacen
+                    partner.l10n_br_city_id.country_id.codigo_bacen
                 dest.enderDest.xPais.valor = \
-                    partner.municipio_id.pais_id.nome
+                    partner.l10n_br_city_id.country_id.name
 
         dest.enderDest.fone.valor = limpa_formatacao(partner.phone or '')
-        email_dest = partner.email_nfe or ''
+        email_dest = partner.email or ''
         dest.email.valor = email_dest[:60]
 
     def _monta_nfe_endereco_retirada(self, retirada):
@@ -289,14 +292,16 @@ class SpedDocumento(models.Model):
         if not self.endereco_retirada_id:
             return
 
-        retirada.xLgr.valor = self.endereco_retirada_id.endereco or ''
-        retirada.nro.valor = self.endereco_retirada_id.numero or ''
-        retirada.xCpl.valor = self.endereco_retirada_id.complemento or ''
-        retirada.xBairro.valor = self.endereco_retirada_id.bairro or ''
-        retirada.cMun.valor = \
-            self.endereco_retirada_id.municipio_id.codigo_ibge[:7]
-        retirada.xMun.valor = self.endereco_retirada_id.municipio_id.nome
-        retirada.UF.valor = self.endereco_retirada_id.municipio_id.estado_id.uf
+        retirada.xLgr.valor = self.endereco_retirada_id.street or ''
+        retirada.nro.valor = self.endereco_retirada_id.number or ''
+        retirada.xCpl.valor = self.endereco_retirada_id.street2 or ''
+        retirada.xBairro.valor = self.endereco_retirada_id.district or ''
+        retirada.cMun.valor = ('%s%s') % (
+            self.endereco_retirada_id.state_id.ibge_code,
+            self.endereco_retirada_id.l10n_br_city_id.ibge_code
+        )
+        retirada.xMun.valor = self.endereco_retirada_id.city
+        retirada.UF.valor = self.endereco_retirada_id.estado
 
         if self.endereco_retirada_id.cpf:
             if len(self.endereco_retirada_id.cpf) == 18:
@@ -311,14 +316,16 @@ class SpedDocumento(models.Model):
         if not self.endereco_entrega_id:
             return
 
-        entrega.xLgr.valor = self.endereco_entrega_id.endereco or ''
-        entrega.nro.valor = self.endereco_entrega_id.numero or ''
-        entrega.xCpl.valor = self.endereco_entrega_id.complemento or ''
-        entrega.xBairro.valor = self.endereco_entrega_id.bairro or ''
-        entrega.cMun.valor = \
-            self.endereco_entrega_id.municipio_id.codigo_ibge[:7]
-        entrega.xMun.valor = self.endereco_entrega_id.municipio_id.nome
-        entrega.UF.valor = self.endereco_entrega_id.municipio_id.estado_id.uf
+        entrega.xLgr.valor = self.endereco_entrega_id.street or ''
+        entrega.nro.valor = self.endereco_entrega_id.number or ''
+        entrega.xCpl.valor = self.endereco_entrega_id.street2 or ''
+        entrega.xBairro.valor = self.endereco_entrega_id.district or ''
+        entrega.cMun.valor = ('%s%s') % (
+            self.endereco_entrega_id.state_id.ibge_code,
+            self.endereco_entrega_id.l10n_br_city_id.ibge_code
+        )
+        entrega.xMun.valor = self.endereco_entrega_id.city
+        entrega.UF.valor = self.endereco_entrega_id.estado
 
         if self.endereco_entrega_id.cnpj_cpf:
             if len(self.endereco_entrega_id.cnpj_cpf) == 18:
@@ -351,16 +358,16 @@ class SpedDocumento(models.Model):
                     limpa_formatacao(self.transportadora_id.cnpj_cpf)
 
             transp.transporta.xNome.valor = \
-                self.transportadora_id.razao_social or ''
+                self.transportadora_id.legal_name or ''
             transp.transporta.IE.valor = \
-                limpa_formatacao(self.transportadora_id.ie or 'ISENTO')
-            ender = self.transportadora_id.endereco or ''
+                limpa_formatacao(self.transportadora_id.inscr_est or 'ISENTO')
+            ender = self.transportadora_id.street or ''
             ender += ' '
-            ender += self.transportadora_id.numero or ''
+            ender += self.transportadora_id.number or ''
             ender += ' '
-            ender += self.transportadora_id.complemento or ''
+            ender += self.transportadora_id.street2 or ''
             transp.transporta.xEnder.valor = ender.strip()
-            transp.transporta.xMun.valor = self.transportadora_id.cidade or ''
+            transp.transporta.xMun.valor = self.transportadora_id.city or ''
             transp.transporta.UF.valor = self.transportadora_id.estado or ''
 
         if self.veiculo_id:
@@ -413,7 +420,9 @@ class SpedDocumento(models.Model):
             vol.nVol.valor = ''
             vol.pesoL.valor = str(
                 D(self.peso_liquido or 0).quantize(D('0.001')))
-            vol.pesoB.valor = str(D(self.peso_bruto or 0).quantize(D('0.001')))
+            vol.pesoB.valor = str(
+                D(self.peso_bruto or 0).quantize(D('0.001'))
+            )
 
             transp.vol.append(vol)
         else:

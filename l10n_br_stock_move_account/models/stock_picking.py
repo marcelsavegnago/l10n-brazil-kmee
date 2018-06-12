268d280dcc0dafc7fdf4ffb01d074de105ae633f
Luiz Felipe do Divino <lf.divino@gmail.com>
2018-06-12 16:50:11 -0300

    [ADD] Relacionamento entre o stock.picking e o account.move

--- l10n_br_stock_move_account/models/stock_picking.py
+++ l10n_br_stock_move_account/models/stock_picking.py
@@ -2,45 +2,16 @@
 # Copyright (C) 2018  Luiz Felipe do Divino - ABGF - luiz.divino@abgf.gov.br
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import api, fields, models
-
-
-STATE = [
-    ('draft', 'Rascunho'),
-    ('cancel', 'Cancelado'),
-    ('waiting', 'Esperando outra operação'),
-    ('confirmed', 'Aguardando disponibilidade'),
-    ('partially_available', 'Parcialmente Disponível'),
-    ('provisorio', 'Recebimento Provisório'),
-    ('assigned', 'Aguardando recebimento'),
-    ('done', 'Recebido'),
-]
+from openerp import api, models, fields
 
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    state = fields.Selection(
-        help="",
-        selection=STATE,
-    )
-    code = fields.Selection(
-        related='picking_type_id.code',
-        store=True,
-    )
-    status = fields.Boolean(
-        compute='get_status',
-    )
-
-    temporary_received_journal_id = fields.Many2one(
-        comodel_name='account.journal',
-        string='Diário Recebimento Provisório'
-    )
-
-    temporary_received_ids = fields.One2many(
-        comodel_name='stock.picking.temporary',
-        inverse_name='stock_picking_id',
-        string='Recebimentos Provisórios'
+    account_move_ids = fields.One2many(
+        string='Movimentações Contábeis',
+        comodel_name='account.move',
+        inverse_name='picking_id',
     )
 
     @api.multi

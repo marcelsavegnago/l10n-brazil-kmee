86757be145c74669bb215e7d6c832c1d793f6de2
Luiz Felipe do Divino <lf.divino@gmail.com>
2018-06-13 15:26:04 -0300

    [ADD] Funcionalidade para criar a movimentação temporária
    Na contabilidade utilizando o modelo de partidas dobradas selecionada
    na empresa.

--- l10n_br_stock_move_account/models/stock_picking.py
+++ l10n_br_stock_move_account/models/stock_picking.py
@@ -3,16 +3,37 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from openerp import api, models, fields
+from openerp.exceptions import Warning
+from openerp.addons.l10n_br_account_product.constantes import \
+    ACCOUNT_AUTOMATICO_PRODUTO
 
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
+
+    journal_id = fields.Many2one(
+        string='Diário',
+        comodel_name='account.journal',
+    )
+
+    period_id = fields.Many2one(
+        string='Periodo',
+        comodel_name='account.period',
+    )
 
     account_move_ids = fields.One2many(
         string='Movimentações Contábeis',
         comodel_name='account.move',
         inverse_name='picking_id',
     )
+
+    @api.multi
+    def action_confirm(self):
+        stock_id = super(StockPicking, self).action_confirm()
+
+        self.gera_movimentacao_contabil_transitoria()
+
+        return stock_id
 
     @api.multi
     def action_provisorio(self):
@@ -34,3 +55,109 @@ class StockPicking(models.Model):
             is_incoming = True
         self.status = (is_incoming and not is_done) or \
                       (is_draft and not is_incoming)
+
+    @api.multi
+    def gera_movimentacao_contabil_transitoria(self):
+        for stock in self:
+            if stock.code != 'incoming':
+                return
+
+            if stock.company_id.active_stock_move_account:
+                move_template = stock.company_id.account_move_template_id
+                self._validar_configuracoes_movimentacao_transitoria(
+                    move_template, stock
+                )
+
+                account_move_lines = []
+
+                self.gera_account_move_line(
+                    account_move_lines, move_template, stock
+                )
+
+                if account_move_lines:
+                    move_vals = self.get_account_move_stock_vals(
+                        account_move_lines, stock)
+
+                    self.env['account.move'].create(move_vals)
+
+    def get_account_move_stock_vals(self, account_move_lines, stock):
+        move_vals = {
+            'ref': stock.name,
+            'line_id': account_move_lines,
+            'journal_id': stock.journal_id.id,
+            'partner_id': stock.partner_id.id,
+            'date': stock.date,
+            'company_id': stock.company_id.id,
+            'picking_id': stock.id,
+        }
+        return move_vals
+
+    def gera_account_move_line(self, account_move_lines, move_template, stock):
+        for line in stock.move_lines:
+            for template_item in move_template.item_ids:
+                if not getattr(line, template_item.campo, False):
+                    continue
+
+                valor_produto = line.purchase_line_id.price_unit if \
+                    line.purchase_line_id else line.product_id.standard_price
+                quantidade_produto = line.product_uom_qty
+                valor_total = valor_produto * quantidade_produto
+
+                account_debito = None
+                if template_item.account_debito_id:
+                    account_debito = template_item.account_debito_id
+                elif template_item.account_automatico_debito == \
+                        ACCOUNT_AUTOMATICO_PRODUTO:
+                    product = line.product_id
+                    account_debito = product.property_account_income
+
+                if account_debito is not None:
+                    dados = {
+                        'account_id': account_debito.id,
+                        # 'move_id': account_move.id,
+                        # 'sped_documento_item_id': self.id,
+                        'name': line.product_id.name,
+                        'narration': template_item.campo,
+                        'debit': valor_total,
+                        # 'currency_id': self.invoice_id.currency_id.id,
+                    }
+                    account_move_lines.append((0, 0, dados))
+
+                account_credito = None
+                if template_item.account_credito_id:
+                    account_credito = template_item.account_credito_id
+
+                elif template_item.account_automatico_credito == \
+                        ACCOUNT_AUTOMATICO_PRODUTO:
+                    product = line.product_id
+                    account_credito = product.property_account_expense
+
+                if account_credito is not None:
+                    dados = {
+                        'account_id': account_credito.id,
+                        # 'move_id': account_move.id,
+                        # 'sped_documento_item_id': self.id,
+                        'name': line.product_id.name,
+                        'narration': template_item.campo,
+                        'credit': valor_total,
+                        # 'currency_id': self.invoice_id.currency_id.id,
+                    }
+                    account_move_lines.append((0, 0, dados))
+
+    def _validar_configuracoes_movimentacao_transitoria(
+            self, move_template, stock):
+        if not move_template:
+            raise Warning(
+                'É necessário escolher um modelo de partida dobrada '
+                'para gerar a movimentação temporária!'
+            )
+        if not stock.journal_id:
+            raise Warning(
+                'É necessário escolher um diário para gerar a '
+                'movimentação temporária!'
+            )
+        if not stock.period_id:
+            raise Warning(
+                'É necessário escolher o periodo de vigencia para '
+                'gerar a movimentação temporária!'
+            )

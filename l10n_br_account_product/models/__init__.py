d34f64d9f18ceea9e9196a2cd2932605b9385b18
Luis Felipe Mileo <mileo@kmee.com.br>
2018-07-23 15:02:42 -0300

    [IMP] Melhorias na forma de pagamento p/ nf-e 4.0
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_account_product/models/__init__.py
+++ l10n_br_account_product/models/__init__.py
@@ -15,3 +15,4 @@ from . import l10n_br_base
 from . import sped_account_move_template_item
 from . import sped_account_move_template
 from . import account_payment
+from . import account_invoice_term

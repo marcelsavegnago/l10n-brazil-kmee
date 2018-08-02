f887c186ee0ae69ac5e7e4bf316a1ec983481ff8
Luis Felipe Mileo <mileo@kmee.com.br>
2018-08-01 21:59:58 -0300

    [REF] Melhorias nos campos do vOrig vDesc vLiq
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_account_product/models/account_invoice.py
+++ l10n_br_account_product/models/account_invoice.py
@@ -47,7 +47,7 @@ class AccountInvoice(models.Model):
         return value
 
     @api.one
-    @api.depends('invoice_line', 'tax_line.amount', 'account_payment_ids.amount')
+    @api.depends('invoice_line', 'tax_line.amount', 'account_payment_line_ids')
     def _compute_amount(self):
         self.icms_base = 0.0
         self.icms_base_other = 0.0
@@ -118,10 +118,13 @@ class AccountInvoice(models.Model):
                 self.icms_st_base += 0.00
                 self.icms_st_value += 0.00
 
+        # Calculando o grupo fatura
+        self.amount_payment_original = sum(p.amount_original for p in self.account_payment_line_ids)
+        self.amount_payment_discount = sum(p.amount_discount for p in self.account_payment_line_ids)
+        self.amount_payment_net = sum(p.amount_net for p in self.account_payment_line_ids)
         # Calculando o troco
-        amount_payments = sum(p.amount for p in self.account_payment_ids)
-        if amount_payments:
-            self.amount_change = amount_payments - self.amount_total
+        if self.amount_payment_net:
+            self.amount_change = self.amount_payment_net - self.amount_total
 
     @api.model
     @api.returns('l10n_br_account.fiscal_category')
@@ -764,7 +767,29 @@ class AccountInvoice(models.Model):
         inverse_name='invoice_id',
     )
     payment_mode_id = fields.Many2one(
-        comodel_name='payment.mode', string="Payment Mode")
+        comodel_name='payment.mode', string="Payment Mode"
+    )
+    amount_payment_original = fields.Float(
+        string='Vr Original',
+        store=True,
+        digits=dp.get_precision('Account'),
+        compute='_compute_amount',
+        help='vOrig'
+    )
+    amount_payment_discount = fields.Float(
+        string='Vr Desconto',
+        store=True,
+        digits=dp.get_precision('Account'),
+        compute='_compute_amount',
+        help='vDesc',
+    )
+    amount_payment_net = fields.Float(
+        string='Vr Liquido',
+        store=True,
+        digits=dp.get_precision('Account'),
+        compute='_compute_amount',
+        help='vLiq',
+    )
 
 
     _sql_constraints = [
@@ -772,6 +797,7 @@ class AccountInvoice(models.Model):
              type, partner_id, document_serie_id, issuer)',
          'Invoice Number must be unique per Company!'),
     ]
+
 
     @api.one
     @api.constrains('number')

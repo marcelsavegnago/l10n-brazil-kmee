e2ac2963d19d7526bcd1351ce800fd4cbc854934
Daniel Sadamo Hirayama <daniel.sadamo@kmee.com.br>
2019-01-22 16:29:41 -0200

    [NEW]Versionamento de anexos nos relatorios MIS

--- l10n_br_contabilidade/models/__init__.py
+++ l10n_br_contabilidade/models/__init__.py
@@ -25,3 +25,5 @@ from . import mis_report_style
 from . import mis_report
 from . import mis_builder
 from . import res_partner
+from . import mis_report_instance
+from . import mis_builder_report_xls

4093ba80f6639a78d3d90711c1dd50b86f1cf464
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2019-01-30 12:16:53 -0200

    [MOV] Módulo abgf_contabilidade para l10n_br_contabilidade

--- specific-parts/specific-addons/abgf_contabilidade/reports/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import general_ledger
-from . import common_reports
-from . import trial_balance
--- specific-parts/specific-addons/abgf_contabilidade/wizards/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import general_ledger_wizard
-from . import fechamento_reabertura_justificativa_wizard
-from . import trial_balance_wizard
-
